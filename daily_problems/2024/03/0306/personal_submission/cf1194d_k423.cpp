a0f14f194ea1459e123e73dbbf9e916e2f0d6690
5cm/s ❀ <cffinity@gmail.com>
2024-03-06 16:48:58 +0800

    submissions of k423

---
+++ daily_problems/2024/03/0306/personal_submission/cf1194d_k423.cpp
@@ -0,0 +1,38 @@
+/**
+ * Created by 5cm/s on 2024/03/06 16:23:07.
+ * 诸天神佛，佑我上分！
+ **/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define itr(it) begin(it), end(it)
+#define endl '\n'
+#define YES() void(cout << "Alice\n")
+#define NO() void(cout << "Bob\n")
+
+void elysia() {
+    int n, k;
+    cin >> n >> k;
+    if (k % 3 == 0) n %= k + 1;
+    n % 3 || n == k ? YES() : NO();
+}
+
+int main() {
+#ifdef MEGURINE
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+    clock_t start = clock();
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int T = 1;
+    cin >> T;
+    cout << fixed << setprecision(12);
+    while (T--) elysia();
+#ifdef MEGURINE
+    cout << "\nRunning Time: " << double(clock() - start) / CLOCKS_PER_SEC * 1000 << "ms" << endl;
+#endif
+    return 0;
+}
