408f08a5775e0b4365c919f8773573c2e0018ee7
Yawn-Sean <1900015431@pku.edu.cn>
2024-03-08 09:17:50 +0800

    20240308 Yawn Sean's 2nd submission for CF895D

---
+++ daily_problems/2024/03/0308/personal_submission/cf895d_yawn_sean_sol2.py
@@ -0,0 +1,243 @@
+standard_input, packages, output_together = 1, 1, 1
+dfs, hashing, read_from_file = 0, 0, 0
+de = 1
+
+if 1:
+
+    if standard_input:
+        import io, os, sys
+        input = lambda: sys.stdin.readline().strip()
+
+        import math
+        inf = math.inf
+
+        def I():
+            return input()
+        
+        def II():
+            return int(input())
+
+        def MII():
+            return map(int, input().split())
+
+        def LI():
+            return list(input().split())
+
+        def LII():
+            return list(map(int, input().split()))
+
+        def LFI():
+            return list(map(float, input().split()))
+
+        def GMI():
+            return map(lambda x: int(x) - 1, input().split())
+
+        def LGMI():
+            return list(map(lambda x: int(x) - 1, input().split()))
+
+    if packages:
+        from io import BytesIO, IOBase
+
+        import random
+        import os
+
+        import bisect
+        import typing
+        from collections import Counter, defaultdict, deque
+        from copy import deepcopy
+        from functools import cmp_to_key, lru_cache, reduce
+        from heapq import merge, heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+        from itertools import accumulate, combinations, permutations, count, product
+        from operator import add, iand, ior, itemgetter, mul, xor
+        from string import ascii_lowercase, ascii_uppercase, ascii_letters
+        from typing import *
+        BUFSIZE = 4096
+
+    if output_together:
+        class FastIO(IOBase):
+            newlines = 0
+
+            def __init__(self, file):
+                self._fd = file.fileno()
+                self.buffer = BytesIO()
+                self.writable = "x" in file.mode or "r" not in file.mode
+                self.write = self.buffer.write if self.writable else None
+
+            def read(self):
+                while True:
+                    b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+                    if not b:
+                        break
+                    ptr = self.buffer.tell()
+                    self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+                self.newlines = 0
+                return self.buffer.read()
+
+            def readline(self):
+                while self.newlines == 0:
+                    b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+                    self.newlines = b.count(b"\n") + (not b)
+                    ptr = self.buffer.tell()
+                    self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+                self.newlines -= 1
+                return self.buffer.readline()
+
+            def flush(self):
+                if self.writable:
+                    os.write(self._fd, self.buffer.getvalue())
+                    self.buffer.truncate(0), self.buffer.seek(0)
+
+        class IOWrapper(IOBase):
+            def __init__(self, file):
+                self.buffer = FastIO(file)
+                self.flush = self.buffer.flush
+                self.writable = self.buffer.writable
+                self.write = lambda s: self.buffer.write(s.encode("ascii"))
+                self.read = lambda: self.buffer.read().decode("ascii")
+                self.readline = lambda: self.buffer.readline().decode("ascii")
+
+        sys.stdout = IOWrapper(sys.stdout)
+
+    if dfs:
+        from types import GeneratorType
+
+        def bootstrap(f, stack=[]):
+            def wrappedfunc(*args, **kwargs):
+                if stack:
+                    return f(*args, **kwargs)
+                else:
+                    to = f(*args, **kwargs)
+                    while True:
+                        if type(to) is GeneratorType:
+                            stack.append(to)
+                            to = next(to)
+                        else:
+                            stack.pop()
+                            if not stack:
+                                break
+                            to = stack[-1].send(to)
+                    return to
+            return wrappedfunc
+
+    if hashing:
+        RANDOM = random.getrandbits(20)
+        class Wrapper(int):
+            def __init__(self, x):
+                int.__init__(x)
+
+            def __hash__(self):
+                return super(Wrapper, self).__hash__() ^ RANDOM
+
+    if read_from_file:
+        file = open("input.txt", "r").readline().strip()[1:-1]
+        fin = open(file, 'r')
+        input = lambda: fin.readline().strip()
+        output_file = open("output.txt", "w")
+        def fprint(*args, **kwargs):
+            print(*args, **kwargs, file=output_file)
+
+    if de:
+        def debug(*args, **kwargs):
+            print('\033[92m', end='')
+            print(*args, **kwargs)
+            print('\033[0m', end='')
+
+class Factorial:
+    def __init__(self, N, mod) -> None:
+        N += 1
+        self.mod = mod
+        self.f = [1 for _ in range(N)]
+        self.g = [1 for _ in range(N)]
+        for i in range(1, N):
+            self.f[i] = self.f[i - 1] * i % self.mod
+        self.g[-1] = pow(self.f[-1], mod - 2, mod)
+        for i in range(N - 2, -1, -1):
+            self.g[i] = self.g[i + 1] * (i + 1) % self.mod
+
+    def fac(self, n):
+        return self.f[n]
+
+    def fac_inv(self, n):
+        return self.g[n]
+
+    def combi(self, n, m):
+        if n < m or m < 0 or n < 0: return 0
+        return self.f[n] * self.g[m] % self.mod * self.g[n - m] % self.mod
+
+    def permu(self, n, m):
+        if n < m or m < 0 or n < 0: return 0
+        return self.f[n] * self.g[n - m] % self.mod
+
+    def catalan(self, n):
+        return (self.combi(2 * n, n) - self.combi(2 * n, n - 1)) % self.mod
+
+    def inv(self, n):
+        return self.f[n-1] * self.g[n] % self.mod
+
+mod = 10 ** 9 + 7
+fact = Factorial(10 ** 6, mod)
+
+class FenwickTree:
+    def __init__(self, nums):
+        self.bit = nums[:]
+        self.n = len(nums)
+        for i in range(self.n):
+            if i | (i + 1) < self.n:
+                self.bit[i | (i + 1)] += self.bit[i]
+
+    def sum(self, r):
+        res = 0
+        while r >= 0:
+            res += self.bit[r]
+            r = (r & (r + 1)) - 1
+        return res
+
+    def rsum(self, l, r):
+        return self.sum(r) - self.sum(l - 1)
+
+    def add(self, idx, delta):
+        while idx < self.n:
+            self.bit[idx] += delta
+            idx = idx | (idx + 1)
+
+def main():
+    s1 = [ord(c) - ord('a') for c in I()]
+    s2 = [ord(c) - ord('a') for c in I()]
+    n = len(s1)
+    
+    cnt = [0] * 26
+    for c in s1:
+        cnt[c] += 1
+    
+    orig = fact.fac(n)
+    for c in cnt:
+        orig *= fact.fac_inv(c)
+        orig %= mod
+    
+    def f(s):
+        fen = FenwickTree(cnt)
+        cur = orig
+        
+        ans = 0
+        for i in range(n):
+            cur *= fact.inv(n - i)
+            cur %= mod
+            
+            ans += cur * fen.sum(s[i] - 1)
+            ans %= mod
+            
+            v = fen.rsum(s[i], s[i])
+            if v == 0: break
+            
+            cur *= v
+            cur %= mod
+            fen.add(s[i], -1)
+        
+        return ans
+    
+    print((f(s2) - f(s1) - 1) % mod)
+    return
+
+t = 1
+for _ in range(t):
+    main()
