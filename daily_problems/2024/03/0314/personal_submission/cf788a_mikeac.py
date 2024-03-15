f7e7eb686f97c252fc8d22f8ba72c8f27c2a5522
Yawn-Sean <106463906+Yawn-Sean@users.noreply.github.com>
2024-03-15 08:54:47 +0800

    20240314

--- daily_problems/2024/03/0314/personal_submission/cf788a_mikeac.py
+++ daily_problems/2024/03/0314/personal_submission/cf788a_mikeac.py
@@ -7,7 +7,6 @@ sint = lambda: int(input())
 mint = lambda: map(int, input().split())
 ints = lambda: list(map(int, input().split()))
 # endregion fastio
-
 # MOD = 998_244_353
 # MOD = 10 ** 9 + 7
 # DIR4 = ((-1, 0), (0, 1), (1, 0), (0, -1)) #URDL
