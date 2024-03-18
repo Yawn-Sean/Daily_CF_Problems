e0d44c6bf38a16314d7dee994982f958bab5a4e4
anthony-zhang <80021042+YinqiaoZhang@users.noreply.github.com>
2024-03-18 16:39:45 +0800

    retyrn's submission for 20240318

---
+++ daily_problems/2024/03/0318/personal_submission/cf863d_retyrn.cpp
@@ -0,0 +1,136 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <cassert>
+#include <cctype>
+#include <ciso646>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#if __cplusplus >= 201103L
+#include <array>
+#include <chrono>
+#include <random>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+#define FAST ios::sync_with_stdio(false);cin.tie(0),cout.tie(0);cout<<fixed<<setprecision(15)
+#define Tsolve() int T; cin >> T; while (T --) solve()
+#define endl '\n'
+#define em(x) (x.empty())
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(),(x).end()
+#define mem(a,b) memset(a,b,sizeof(a))
+#define Fill(x,a) memset(x,a,sizeof(x))
+#define cpy(a,b) memcpy(a,b,sizeof(a))
+#define pbk push_back
+#define mpr make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define rep(i,a,n) for(int i=a;i<=n;++i)
+#define fi first
+#define se second
+#define complete_unique(a) a.erase(unique(a.begin(),a.end()),a.end())
+//-----------------------precompiler--------------------
+using ll = long long;
+using i64 = long long;
+using pii = std::pair<int,int>;
+using VI = std::vector<int>;
+using namespace std;
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+//-----------------------debug-----------------------
+#ifndef ONLINE_JUDGE
+#include "D:/OneDrive-Personal/OneDrive/my-acm-template/my_header/debug.h"
+#define dbg(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define dbg(...) {}
+#endif
+//-----------------------constant-----------------------
+const int maxn = 2e5+10;
+const int inf = 0x3f3f3f3f;
+const ll inf2 = 0x3f3f3f3f3f3f3f3f;
+const double eps = 1e-8;
+const int mod = 1e9+7;
+const int mod2 = 998244353;
+ll gcd(ll a,ll b) { return b?gcd(b, a%b):a;}
+//-----------------------variable-----------------------
+int n, q, m;
+using ai3 = array<int, 3>;
+//-----------------------function-----------------------
+
+void solve() {
+    cin >> n >> q >> m;
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i ++) {
+        cin >> a[i];
+    }
+    vector<ai3> b(q);
+    for (int i = 0; i < q; i ++) {
+        cin >> b[i][0] >> b[i][1] >> b[i][2];
+    }
+    
+    vector<int> id(m);
+    cin >> id;
+    
+    for (auto x : id) {
+        for (int i = q - 1; i >= 0; i --) {
+            auto [op, l, r] = b[i];
+            if (op == 1) {
+                // 右移
+                if (l <= x and x <= r) {
+                    if (x == l) x = r;
+                    else x -= 1;
+                }
+            }
+            else {
+                // reverse
+                if (l <= x and x <= r) {
+                    x = l + r - x;
+                }
+            }
+        }
+        cout << a[x] << ' ';
+    }
+    cout << endl;
+}
+int main() {
+    FAST;
+    // Tsolve();
+    solve();
+
+    return 0;
+}
+
+// 3 2 1 4 5 6
+// 3 2 6 5 4 1
+// 1 3 2 6 5 4
+
+// 64 3 4 665 2
+// 4 64 3 665 2
+// 2 665 3 64 4
