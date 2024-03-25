d38646fedd74102e2639d7bb570618687d1c4d96
anthony-zhang <80021042+YinqiaoZhang@users.noreply.github.com>
2024-03-25 23:32:03 +0800

    Update cf963a_retyrn.cpp

--- daily_problems/2024/03/0325/personal_submission/cf963a_retyrn.cpp
+++ daily_problems/2024/03/0325/personal_submission/cf963a_retyrn.cpp
@@ -82,40 +82,136 @@ ll gcd(ll a,ll b) { return b?gcd(b, a%b):a;}
 ll n, a, b, k;
 string s;
 //-----------------------function-----------------------
-ll powmod (ll a, ll b) {
-    ll res = 1;
-    a %= mod;
-    assert(b >= 0);
-    for(; b; b >>= 1) {
-        if (b & 1) res = res * a % mod;
-        a = a * a % mod;
+template<class T>
+constexpr T power(T a, i64 b) {
+    T res = 1;
+    for (; b; b /= 2, a *= a) {
+        if (b % 2) {
+            res *= a;
+        }
     }
     return res;
 }
+template<int P>
+struct MInt {
+    int x;
+    constexpr MInt() : x{} {}
+    constexpr MInt(i64 x) : x{norm(x % getMod())} {}
+    
+    static int Mod;
+    constexpr static int getMod() {
+        if (P > 0) {
+            return P;
+        } else {
+            return Mod;
+        }
+    }
+    constexpr static void setMod(int Mod_) {
+        Mod = Mod_;
+    }
+    constexpr int norm(int x) const {
+        if (x < 0) {
+            x += getMod();
+        }
+        if (x >= getMod()) {
+            x -= getMod();
+        }
+        return x;
+    }
+    constexpr int val() const {
+        return x;
+    }
+    explicit constexpr operator int() const {
+        return x;
+    }
+    constexpr MInt operator-() const {
+        MInt res;
+        res.x = norm(getMod() - x);
+        return res;
+    }
+    constexpr MInt inv() const {
+        assert(x != 0);
+        return power(*this, getMod() - 2);
+    }
+    constexpr MInt &operator*=(MInt rhs) & {
+        x = 1LL * x * rhs.x % getMod();
+        return *this;
+    }
+    constexpr MInt &operator+=(MInt rhs) & {
+        x = norm(x + rhs.x);
+        return *this;
+    }
+    constexpr MInt &operator-=(MInt rhs) & {
+        x = norm(x - rhs.x);
+        return *this;
+    }
+    constexpr MInt &operator/=(MInt rhs) & {
+        return *this *= rhs.inv();
+    }
+    friend constexpr MInt operator*(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res *= rhs;
+        return res;
+    }
+    friend constexpr MInt operator+(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res += rhs;
+        return res;
+    }
+    friend constexpr MInt operator-(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res -= rhs;
+        return res;
+    }
+    friend constexpr MInt operator/(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res /= rhs;
+        return res;
+    }
+    friend constexpr std::istream &operator>>(std::istream &is, MInt &a) {
+        i64 v;
+        is >> v;
+        a = MInt(v);
+        return is;
+    }
+    friend constexpr std::ostream &operator<<(std::ostream &os, const MInt &a) {
+        return os << a.val();
+    }
+    friend constexpr bool operator==(MInt lhs, MInt rhs) {
+        return lhs.val() == rhs.val();
+    }
+    friend constexpr bool operator!=(MInt lhs, MInt rhs) {
+        return lhs.val() != rhs.val();
+    }
+};
+ 
+template<>
+int MInt<0>::Mod = 1;
+ 
+template<int V, int P>
+constexpr MInt<P> CInv = MInt<P>(V).inv();
+ 
+constexpr int P = mod;
+using Z = MInt<P>;
 
 void solve() {
     cin >> n >> a >> b >> k >> s;
-    // 先算前k个
-    ll res = 0;
-    for (int i = 0; i < k; i ++) {
-        ll val = (s[i] == '+' ? 1 : mod - 1);
-        val = (val * powmod(b, i)) % mod;
-        val = (val * powmod(a, n - i)) % mod;
-        res = (res + val) % mod;
-    }
-    int m = (n + 1) / k;
     
-    ll q = powmod(b, k) * powmod(powmod(a, k), mod - 2) % mod;
-    if (q == 1) {
-        // q == 1 不等于 a == b !!!
-        res = (res * m) % mod;
-        cout << res << endl;
-        return;
+    Z res = 0;
+    for (int i = 0; i < k; i ++) {
+        int t = (s[i] == '+' ? 1 : mod - 1);
+        res += power(Z(a), n - i) * power(Z(b), i) * t;
     }
-
-    ll tmp = res * (mod + 1 - powmod(q, m)) % mod;
-	tmp = tmp * powmod((mod + 1 - q), mod - 2) % mod;
-	res = tmp;
+    
+    int m = (n + 1) / k;
+    Z q = power(Z(b), k) / power(Z(a), k);
+    
+    if (q.val() == 1) {
+        res *= m;
+    }
+    else {
+        res *= (1 - power(q, m)) / (1 - q);
+    }
     cout << res << endl;
 }
 int main() {
