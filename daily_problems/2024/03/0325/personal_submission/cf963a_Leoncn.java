8cb0f07f0a6d8d42540d5a021d484ab4445a3ffe
Leocn <jlj888000@gmail.com>
2024-03-25 17:16:00 +0800

    20240325 Leoncn's submission for CF986B (#599)
    * greedy
    
    * greedy
    
    * Create cf1039b_leoncn.java
    
    * Create cf1260c_Leoncn.java
    
    * Create cf687a_Leoncn.java
    
    * Create cf1498e_Leoncn.java
    
    * Create cf1512g_Leoncn.java
    
    * Create cf584b_Leoncn.java
    
    * 20240304 Leoncn's submission
    
    * Create cf1083a_Leoncn.java
    
    * Create cf1190d_Leoncn.java
    
    * Create cf1194d_Leoncn.java
    
    * Create cf453a_Leoncn.java
    
    * Create cf1185d_Leoncn.java
    
    * Create cf1025b_Leoncn.java
    
    * Create cf621e_Leoncn.java
    
    * Create cf780b_Leoncn.java
    
    * Create cf1098a_Leoncn.java
    
    * Create cf226d_Leoncn.java
    
    * Create cf827a_Leoncn.java
    
    * 20240314 Leoncn's submission
    
    * Create cf660d_Leoncn.java
    
    * Create cf1076e_Leoncn.java
    
    * 20240316 Leoncn's submission
    
    * Create cf863d_Leoncn.java
    
    * Create cf772b_Leoncn.java
    
    * Create cf773a_Leoncn.java
    
    * Create cf1878f_Leoncn.java
    
    * Create cf375a_Leoncn.java
    
    * Create cf571b_Leoncn.java
    
    * Create cf913c_Leoncn.java
    
    * Create cf491b_Leoncn.java
    
    * Create cf566f_Leoncn.java
    
    * Update cf566f_Leoncn.java
    
    * Create cf1153e_Leoncn.java
    
    * Create cf908c_Leoncn.java
    
    * Create cf1526e_Leoncn.java
    
    * Create cf986b_Leoncn.java
    
    * Create cf963a_Leoncn.java

---
+++ daily_problems/2024/03/0325/personal_submission/cf963a_Leoncn.java
@@ -0,0 +1,209 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.util.*;
+
+/**
+ * @Description A
+ * @Author Leon
+ * @Date 2024/3/25
+ */
+public class A {
+    static RealFastReader sc = new RealFastReader(System.in);
+    static PrintWriter out = new PrintWriter(System.out);
+
+
+    public static void main(String[] args) {
+        int t = 1;
+        while (t-- > 0) {
+            solve();
+        }
+        out.close();
+    }
+
+
+    static int mod = (int)1e9+9;
+    public static void solve() {
+        int n = sc.ni();
+        int a = sc.ni();
+        int b = sc.ni();
+        int k = sc.ni();
+        char[] cs = sc.ns().toCharArray();
+        int f = (n+1)/k;
+        long aInv = pow(a, mod-2, mod);
+        long q = pow( aInv * b%mod, k, mod);
+        long qMinus = pow(q-1, mod-2, mod);
+        long qPow = pow(q, f, mod);
+        long ans = 0;
+        long now = pow(a, n, mod);
+        for (int i = 0; i < k; i++) {
+            int tag = cs[i] =='+'?1:-1;
+            if(q == 1){
+                ans = (ans + tag * f * now%mod)%mod;
+            }else {
+                ans = (ans + tag* now * (qPow-1) %mod * qMinus %mod)%mod;
+            }
+            now = now* aInv %mod * b %mod;
+        }
+        out.println((ans + mod)%mod);
+
+    }
+
+    //快速幂 x^k mod p
+    static long pow(long x, long k, long p) {
+        long res = 1;
+        while (k != 0) {
+            if ((k & 1) != 0) res = res * x % p;
+            x = x * x % p;
+            k >>= 1;
+        }
+        return res;
+    }
+
+
+
+    public static class RealFastReader {
+        InputStream is;
+
+        public RealFastReader(final InputStream is) {
+            this.is = is;
+        }
+
+        private byte[] inbuf = new byte[8192];
+        public int lenbuf = 0, ptrbuf = 0;
+
+        public int readByte() {
+            if (lenbuf == -1) {
+                throw new InputMismatchException();
+            }
+            if (ptrbuf >= lenbuf) {
+                ptrbuf = 0;
+                try {
+                    lenbuf = is.read(inbuf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (lenbuf <= 0) {
+                    return -1;
+                }
+            }
+            return inbuf[ptrbuf++];
+        }
+
+        private boolean isSpaceChar(int c) {
+            return !(c >= 33 && c <= 126);
+        }
+
+        private int skip() {
+            int b;
+            while ((b = readByte()) != -1 && isSpaceChar(b))
+                ;
+            return b;
+        }
+
+        public double nd() {
+            return Double.parseDouble(ns());
+        }
+
+        public char nc() {
+            return (char) skip();
+        }
+
+        public String ns() {
+            int b = skip();
+            StringBuilder sb = new StringBuilder();
+            while (!(isSpaceChar(b))) { // when nextLine, (isSpaceChar(b) && b != ' ')
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
+
+        public char[] ns(int n) {
+            char[] buf = new char[n];
+            int b = skip(), p = 0;
+            while (p < n && !(isSpaceChar(b))) {
+                buf[p++] = (char) b;
+                b = readByte();
+            }
+            return n == p ? buf : Arrays.copyOf(buf, p);
+        }
+
+        public int[] na(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = ni();
+            }
+            return a;
+        }
+
+        public long[] nal(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nl();
+            }
+            return a;
+        }
+
+        public char[][] nm(int n, int m) {
+            char[][] map = new char[n][];
+            for (int i = 0; i < n; i++) {
+                map[i] = ns(m);
+            }
+            return map;
+        }
+
+        public int[][] nmi(int n, int m) {
+            int[][] map = new int[n][];
+            for (int i = 0; i < n; i++) {
+                map[i] = na(m);
+            }
+            return map;
+        }
+
+        public int ni() {
+            int num = 0;
+            int b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+                ;
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = num * 10 + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
+            }
+        }
+
+        public long nl() {
+            long num = 0;
+            int b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+                ;
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = num * 10 + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
+            }
+        }
+    }
+}
