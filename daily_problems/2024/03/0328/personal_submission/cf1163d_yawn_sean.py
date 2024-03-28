9b29123f46e4bb7bd852ca6b131fb74a22f6936c
Yawn-Sean <106463906+Yawn-Sean@users.noreply.github.com>
2024-03-28 09:38:55 +0800

    20240328 Yawn Sean's submission for CF1163D

--- daily_problems/2024/03/0328/personal_submission/cf1163d_yawn_sean.py
+++ daily_problems/2024/03/0328/personal_submission/cf1163d_yawn_sean.py
@@ -9,12 +9,12 @@ def main():
                 j += 1
             pi[i] = j
         return pi
-
+    
     c = I()
-
+    
     s = I()
     t = I()
-
+    
     def get_transition(string):
         n = len(string)
         kmp = prep(string)
@@ -34,30 +34,23 @@ def main():
                         pt -= 1
         
         return transition
-
+    
     a, b = len(s), len(t)
-
+    
     transition_s = get_transition(s)
     transition_t = get_transition(t)
-
+    
     dp = [[-inf] * (b + 1) for _ in range(a + 1)]
     dp[0][0] = 0
     for char in c:
         ndp = [[-inf] * (b + 1) for _ in range(a + 1)]
-        if char == '*':
-            for i in range(a + 1):
-                for j in range(b + 1):
-                    if dp[i][j] != -inf:
-                        for char in range(26):
-                            ni, nj = transition_s[i][char] + 1, transition_t[j][char] + 1
-                            ndp[ni][nj] = max(ndp[ni][nj], dp[i][j])
-        else:
-            char = ord(char) - ord('a')
-            for i in range(a + 1):
-                for j in range(b + 1):
-                    if dp[i][j] != -inf:
-                            ni, nj = transition_s[i][char] + 1, transition_t[j][char] + 1
-                            ndp[ni][nj] = max(ndp[ni][nj], dp[i][j])
+        tmp = ord(char) - ord('a')
+        for i in range(a + 1):
+            for j in range(b + 1):
+                if dp[i][j] != -inf:
+                    for char in (range(26) if tmp < 0 else [tmp]):
+                        ni, nj = transition_s[i][char] + 1, transition_t[j][char] + 1
+                        ndp[ni][nj] = max(ndp[ni][nj], dp[i][j])
         
         for i in range(a + 1):
             ndp[i][b] -= 1
@@ -66,5 +59,5 @@ def main():
             ndp[a][i] += 1
         
         dp = ndp
-
+    
     print(max(max(x) for x in dp))
