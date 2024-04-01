669a11ab78d4e8fd0ed0bb5d6ea47c84b56b8bfc
USYDLDH <116334166+USYDLDH@users.noreply.github.com>
2024-04-01 16:39:44 +1100

    Add files via upload

---
+++ daily_problems/2024/04/0401/personal_submission/cf1016d_Ldh.py
@@ -0,0 +1,102 @@
+import sys
+import os
+from io import BytesIO, IOBase
+BUFSIZE = 8192
+class FastIO(IOBase):
+    newlines = 0
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+def I():
+    return input()
+def II():
+    return int(input())
+def MI():
+    return map(int, input().split())
+def LI():
+    return list(input().split())
+def LII():
+    return list(map(int, input().split()))
+def GMI():
+    return map(lambda x: int(x) - 1, input().split())
+
+#------------------------------FastIO---------------------------------
+
+from bisect import *
+from heapq import *
+from collections import *
+from functools import *
+from itertools import *
+from time import *
+from random import *
+from math import log, gcd, sqrt, ceil
+
+inf = float('inf')
+
+
+# @TIME
+def solve(testcase):
+    n, m = MI()
+    A = LII()
+    B = LII()
+    check = 0
+    for a in A:
+        check ^= a
+    for b in B:
+        check ^= b
+    if (check):
+        print("NO")
+        return
+    
+    print("YES")
+    res = [[0 for _ in range(m)] for _ in range(n)]
+    t = 0
+    for j in range(m - 1):
+        res[0][j] = B[j]
+        t ^= B[j]
+    res[0][-1]= A[0] ^ t
+    t = res[0][-1]
+    for i in range(1, n - 1):
+        res[i][-1] = A[i]
+        t ^= A[i]
+    res[-1][-1] = t ^ B[-1]
+
+    for i in range(n):
+        print(*res[i])
+
+for testcase in range(1):
+    solve(testcase)
