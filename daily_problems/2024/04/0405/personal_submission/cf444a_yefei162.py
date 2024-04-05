9240f90bd76df6a22177a0a6da49c3b1c0d86d00
Yawn-Sean <106463906+Yawn-Sean@users.noreply.github.com>
2024-04-05 13:21:15 +0800

    20240405

--- daily_problems/2024/04/0405/personal_submission/cf444a_yefei162.py
+++ daily_problems/2024/04/0405/personal_submission/cf444a_yefei162.py
@@ -1,17 +1,3 @@
-# -*- coding : utf-8 -*-
-import sys
-
-input = lambda: sys.stdin.readline().rstrip()
-# sys.stdin = open('../input.txt', 'r')
-I = lambda: int(input())
-MI = lambda: map(int, input().split())
-GMI = lambda: map(lambda x: int(x) - 1, input().split())
-LI = lambda: list(MI())
-LGMI = lambda: list(GMI())
-YN = lambda x: print('YES' if x else 'NO')
-mod = 1000000007
-mod2 = 998244353
-
 n, m = MI()
 nums = LI()
 mx = 0
