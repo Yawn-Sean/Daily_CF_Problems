f0704359fe66b885530304d87175d8a90364f278
mdwang <mengdiwang@users.noreply.github.com>
2024-04-13 00:22:57 -0700

    remove lib code

--- daily_problems/2024/04/0413/personal_submission/cf830b_md.cpp
+++ daily_problems/2024/04/0413/personal_submission/cf830b_md.cpp
@@ -1,43 +1,6 @@
 #include <bits/stdc++.h>
-#include <ranges>
 using namespace std;
-
 using i64 = long long;
-template <typename T>
-struct Fenwick {
-    int n;
-    std::vector<T> a;
-    
-    Fenwick(int n = 0) {
-        init(n);
-    }
-    
-    void init(int n) {
-        this->n = n;
-        a.assign(n, T());
-    }
-    
-    // set num[x] = v
-    void add(int x, T v) {
-        for (int i = x + 1; i <= n; i += i & -i) {
-            a[i - 1] += v;
-        }
-    }
-
-    // sum of [0, x)    
-    T sum(int x) {
-        auto ans = T();
-        for (int i = x; i > 0; i -= i & -i) {
-            ans += a[i - 1];
-        }
-        return ans;
-    }
-    
-    // rangeSum [l, r)
-    T rangeSum(int l, int r) {
-        return sum(r) - sum(l);
-    }
-};
 
 int main() {
     std::ios::sync_with_stdio(false);
