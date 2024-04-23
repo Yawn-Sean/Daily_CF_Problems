e78db46382637c98c321edd875d97306fb390ef4
Yawn-Sean <106463906+Yawn-Sean@users.noreply.github.com>
2024-04-23 22:03:01 +0800

    20240423

--- daily_problems/2024/04/0423/personal_submission/cf922d_yefei162.py
+++ daily_problems/2024/04/0423/personal_submission/cf922d_yefei162.py
@@ -1,18 +1,3 @@
-# -*- coding : utf-8 -*-
-import sys
-from functools import cmp_to_key
-
-input = lambda: sys.stdin.readline().rstrip()
-# sys.stdin = open('../../input.txt', 'r')
-I = lambda: int(input())
-MI = lambda: map(int, input().split())
-GMI = lambda: map(lambda x: int(x) - 1, input().split())
-LI = lambda: list(MI())
-LGMI = lambda: list(GMI())
-YN = lambda x: print('YES' if x else 'NO')
-mod = 1000000007
-mod2 = 998244353
-
 
 def cmp(p0, p1):
     s0, h0 = p0
