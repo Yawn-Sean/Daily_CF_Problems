38a7c49a1118eb27211d0fc64a98e8bb78e7cdb3
Yawn-Sean <106463906+Yawn-Sean@users.noreply.github.com>
2024-05-01 18:25:19 +0800

    20240501

--- daily_problems/2024/04/0430/personal_submission/cf747d_dwq.java
+++ daily_problems/2024/04/0430/personal_submission/cf747d_dwq.java
@@ -1,7 +1,6 @@
 import java.util.*;
 import java.io.*;
 public class Test{
-    
     public static void main(String[] args) throws Exception{
         
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
