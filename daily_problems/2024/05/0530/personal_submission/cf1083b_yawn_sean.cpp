61856d9ca36d56020b5fc1128018028749110b55
Yawn-Sean <106463906+Yawn-Sean@users.noreply.github.com>
2024-05-30 15:30:09 +0800

    20240530 Yawn Sean's submission for CF1083B

--- daily_problems/2024/05/0530/personal_submission/cf1083b_yawn_sean.cpp
+++ daily_problems/2024/05/0530/personal_submission/cf1083b_yawn_sean.cpp
@@ -1,23 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct custom_hash
-{
-    static uint64_t splitmix64(uint64_t x)
-    {
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-
-    size_t operator()(uint64_t x) const
-    {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
-};
-
 signed main()
 {
     ios_base::sync_with_stdio(false);
