5b8b540bf6905679b4a8996eb7f11b01b616ce2a
Equinox <86874660+58164@users.noreply.github.com>
2024-06-08 21:41:58 +0800

    cf

--- daily_problems/2024/06/0608/personal_submission/cf1056b_Equinox.cpp
+++ daily_problems/2024/06/0608/personal_submission/cf1056b_Equinox.cpp
@@ -10,7 +10,26 @@ std::ostream& operator<< (std::ostream& out, i128 x) {
 }
 
 void solve() {
-    
+    i64 res = 0, N, M;
+    std::cin >> N >> M;
+    for (int i = 1; i <= M; i ++ )
+        for (int j = 1; j <= M; j ++ ) 
+            if ((i * i + j * j) % M == 0) {
+                i64 cnt_i = (N - i + M) / M, cnt_j = (N - j + M) / M;
+                res += cnt_i * cnt_j;
+            }
+    std::cout << res;
+    /*
+        a^2 + b^2 | m
+        (a mod m)^2 + (b mod m)^2 | m
+
+        x^2 + y^2 | m
+        Σ(cnt_x * cnt_y)
+
+        m(q - 1) + r <= n
+        q = (n - r + m) / m
+
+    */
 }
 
 
