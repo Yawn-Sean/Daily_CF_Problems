224b12da4775d721880415e669ed1a4993a6fd42
Amani <Kangyan@stu.sicau.edu.cn>
2024-06-11 17:05:53 +0800

    Update
    Update cf725d_Amani.cpp

--- daily_problems/2024/06/0611/personal_submission/cf725d_Amani.cpp
+++ daily_problems/2024/06/0611/personal_submission/cf725d_Amani.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int Rank(int n, int x, vector<pair<int, int>>& b_w) {
-    int left = 0, right = n - 1;
+    int left = 0, right = n - 2;
     while (left <= right) {
         int mid = left + (right - left) / 2;
         if (b_w[mid].first > x) {
@@ -17,13 +17,13 @@ int Rank(int n, int x, vector<pair<int, int>>& b_w) {
 
 int Solve(int n, vector<pair<int, int>>& b_w, int ball_1) {
     priority_queue<int, vector<int>, greater<int>> pq;
-    sort(b_w.begin()+1, b_w.end(), [&](pair<int, int>& a, pair<int, int>& b) {
+    sort(b_w.begin(), b_w.end(), [&](pair<int, int>& a, pair<int, int>& b) {
         return a.first > b.first;
     });
     int ans = Rank(n, ball_1, b_w);
     int pre = ans;  // 记录上一次的排名
     int cnt = 0;
-    for (int i = 1; i < pre; ++i) {  // 可能要贿赂的队伍入队
+    for (int i = 0; i < pre; ++i) {  // 可能要贿赂的队伍入队
         pq.push(b_w[i].second);
     }
     while (!pq.empty()) {
@@ -34,8 +34,8 @@ int Solve(int n, vector<pair<int, int>>& b_w, int ball_1) {
         pq.pop();
         cnt++;
         ball_1 -= w;
-        int cur = Rank(n, ball_1, b_w);
-        for (int i = pre; i < cur; ++i) {
+        int cur = Rank(n, ball_1, b_w);  // ball_1减少后，新的排名
+        for (int i = pre; i < cur; ++i) {  // 加入超过的队伍
             pq.push(b_w[i].second);
         }
         pre = cur;
@@ -51,14 +51,13 @@ signed main() {
 
     int n;
     cin >> n;
-    vector<pair<int, int>> b_w(n);
+    vector<pair<int, int>> b_w(n-1);
     int ball_1, weight_1;
     cin >> ball_1 >> weight_1;
-    b_w[0] = {ball_1, weight_1};
-    for (int i = 1; i < n; ++i) {
+    for (int i = 0; i < n-1; ++i) {
         cin >> b_w[i].first >> b_w[i].second;
         b_w[i].second -= b_w[i].first - 1;  // 修正 weights，当 weights = 0 时，直接漂浮
     }
-    cout << max(1LL, Solve(n, b_w, ball_1)) << endl;
+    cout << Solve(n, b_w, ball_1) + 1 << endl;  // 排名偏差 +1
     return 0;
 }
