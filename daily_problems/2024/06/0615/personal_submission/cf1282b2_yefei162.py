c6889b38607c9c7e73dd10582b19a8e6e5fc6083
wyf <1626607072@qq.com>
2024-06-15 11:49:58 +0800

    20240615 Yefei162's submission for cf1282b2

--- daily_problems/2024/06/0615/personal_submission/cf1282b2_yefei162.py
+++ daily_problems/2024/06/0615/personal_submission/cf1282b2_yefei162.py
@@ -2,8 +2,6 @@
 import sys
 
 input = lambda: sys.stdin.readline().rstrip()
-sys.stdin = open('../../input.txt', 'r')
-sys.stdout = open('../../output.txt', 'w')
 I = lambda: int(input())
 MI = lambda: map(int, input().split())
 GMI = lambda: map(lambda x: int(x) - 1, input().split())
