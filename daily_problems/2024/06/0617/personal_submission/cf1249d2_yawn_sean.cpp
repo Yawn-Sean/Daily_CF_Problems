824bd47e4453b38699b7d8778b0b9722ba0931ac
Yawn-Sean <1900015431@pku.edu.cn>
2024-06-17 11:51:30 +0800

    Editorial for CF1249D2

--- daily_problems/2024/06/0617/personal_submission/cf1249d2_yawn_sean.cpp
+++ daily_problems/2024/06/0617/personal_submission/cf1249d2_yawn_sean.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 signed main()
 {
-    // ios_base::sync_with_stdio(false);
-    // cin.tie(0);
-    // cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
 
     int n, k;
     cin >> n >> k;
