87c9100836c1eda3fb7b6ceaf03254139a1f0e59
ng4seon3fai1 <1226178895@qq.com>
2024-06-20 10:47:24 +0800

    20240620 Update cf678d_ncf.go

--- daily_problems/2024/06/0620/personal_submission/cf678d_ncf.go
+++ daily_problems/2024/06/0620/personal_submission/cf678d_ncf.go
@@ -1,113 +1,79 @@
-# -*- coding: utf-8 -*-
-# @Project: leetcode
-# @File    : __init__.py.py
-# @Author  : Ng_Chun-fai
-# @Time    : 2024-06-16 10:23:00
+package main
 
-from bisect import bisect_left, bisect_right
-from collections import deque, Counter, defaultdict
-from functools import cache
-from itertools import pairwise
-from math import inf
-from typing import List, Optional
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
 
-from sortedcontainers import SortedList
+func main() { CF678D(os.Stdin, os.Stdout) }
 
+func CF678D(_r io.Reader, out io.Writer) {
+	in := bufio.NewReader(_r)
+	var a, b, x, n int
+	fmt.Fscan(in, &a, &b, &n, &x)
 
-class Fenwick:
-    __slots__ = 'tree'
+	m1, m2, res := make([][]int, 2), make([][]int, 2), make([][]int, 2)
+	for i := 0; i < 2; i++ {
+		m1[i] = make([]int, 2)
+		m2[i] = make([]int, 1)
+		res[i] = make([]int, 2)
+	}
 
-    def __init__(self, n: int):
-        self.tree = [0] * n
+	m1[0][0] = a
+	m1[0][1] = b
+	m1[1][0] = 0
+	m1[1][1] = 1
+	m2[0][0] = x
+	m2[1][0] = 1
+	res[0][0] = 1
+	res[0][1] = 0
+	res[1][0] = 0
+	res[1][1] = 1
 
-    # 把下标为 i 的元素增加 1
-    def add(self, i: int, val: int) -> None:
-        while i < len(self.tree):
-            self.tree[i] += val
-            i += i & -i
+	mod := int(1e9 + 7)
 
-    # 返回下标在 [1,i] 的元素之和
-    def pre(self, i: int) -> int:
-        res = 0
-        while i > 0:
-            res += self.tree[i]
-            i &= i - 1
-        return res
+	// 快速幂
+	power := n
+	for power > 0 {
+		if (power & 1) == 1 {
+			res = matrixMultiply(res, m1, mod)
+		}
+		m1 = matrixMultiply(m1, m1, mod)
+		power >>= 1
+	}
+	res = matrixMultiply(res, m2, mod)
+	fmt.Fprintf(out, "%d\n", res[0][0])
+}
 
+// matrixMultiply 计算并返回两个矩阵的乘积。
+func matrixMultiply(a, b [][]int, mod int) [][]int {
+	// 获取矩阵的维度
+	rowsA := len(a)
+	colsA := len(a[0])
+	rowsB := len(b)
+	colsB := len(b[0])
 
-class Solution:
-    def countCompleteDayPairs(self, hours: List[int]) -> int:
-        c = defaultdict(int)
-        ans = 0
-        for e in hours:
-            ans += c[(24 - e % 24) % 24]
-            c[e % 24] += 1
-        return ans
+	// 检查矩阵是否可以相乘
+	if colsA != rowsB {
+		panic("矩阵A的列数必须与矩阵B的行数相同")
+	}
 
-    def maximumTotalDamage(self, power: List[int]) -> int:
-        c = defaultdict(int)
-        for e in power:
-            c[e] += 1
-        li = []
-        for k, v in c.items():
-            li.append((k, v))
-        li.sort()
-        f = [0] * len(li)
-        ans = 0
-        for i, (k, v) in enumerate(li):
-            for j in range(5, 0, -1):
-                if i - j >= 0 and k - li[i - j][0] > 2:
-                    f[i] = max(f[i], f[i - j])
-            f[i] += k * v
-            ans = max(f[i], ans)
-        return ans
+	// 初始化结果矩阵
+	result := make([][]int, rowsA)
+	for i := range result {
+		result[i] = make([]int, colsB)
+	}
 
-    def countOfPeaks(self, nums: List[int], queries: List[List[int]]) -> List[int]:
-        n = len(nums)
-        tr = Fenwick(n + 1)
-        ans = []
+	// 执行矩阵乘法
+	for i := 0; i < rowsA; i++ {
+		for j := 0; j < colsB; j++ {
+			for k := 0; k < colsA; k++ {
+				result[i][j] = (result[i][j] + a[i][k]*b[k][j]) % mod
+			}
+		}
+	}
 
-        def check(idx: int) -> bool:
-            return nums[idx - 1] < nums[idx] and nums[idx] > nums[idx + 1]
-
-        for i in range(1, n - 1):
-            if check(i):
-                tr.add(i, 1)
-
-        for x, y, z in queries:
-            if x == 1:
-                if z - y < 2:
-                    ans.append(0)
-                    continue
-                v = tr.pre(z - 1) - tr.pre(y)
-                ans.append(v)
-            else:
-                v = nums[y]
-                for index in (y - 1, y, y + 1):
-                    if 0 < index < n - 1:
-                        flag = check(index)
-                        nums[y] = z
-                        if check(index) != flag:
-                            if flag:
-                                tr.add(index, -1)
-                            else:
-                                tr.add(index, 1)
-                        nums[y] = v
-                nums[y] = z
-        return ans
-
-
-if __name__ == "__main__":
-    run_code = 0
-    so = Solution()
-    # print(so.countCompleteDayPairs([12, 12, 30, 24, 24]))
-    # print(so.countCompleteDayPairs([72, 48, 24, 3]))
-
-    # print(so.maximumTotalDamage([1, 1, 3, 4]))
-    # print(so.maximumTotalDamage([7, 1, 6, 6]))
-
-    # print(so.countOfPeaks([3, 1, 4, 2, 5], [[2, 3, 4], [1, 0, 4]]))
-    # print(so.countOfPeaks([4, 1, 4, 2, 1, 5], [[2, 2, 4], [1, 0, 2], [1, 0, 4]]))
-    print(so.countOfPeaks([8, 7, 10],
-                          [[1, 1, 1], [2, 2, 4], [1, 0, 1], [2, 1, 9], [1, 0, 2]]))
-    print()
+	return result
+}
