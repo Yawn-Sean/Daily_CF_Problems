7a8b2116b1ff4c099821af2c09019a563844f4a8
Yawn-Sean <1900015431@pku.edu.cn>
2024-06-25 08:28:53 +0800

    20240625 Yawn Sean's submission for CF1032D

--- daily_problems/2024/06/0625/personal_submission/cf1032d_yawn_sean.py
+++ daily_problems/2024/06/0625/personal_submission/cf1032d_yawn_sean.py
@@ -1,38 +1,18 @@
-# Submission Link: https://codeforces.com/contest/863/submission/267242134
+# Submission Link: https://codeforces.com/contest/1032/submission/267242403
 def main():
-    k, a, b = MII()
+    def dis(x, y):
+        return ((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2) ** 0.5
 
-    grid1 = [LGMI() for _ in range(3)]
-    grid2 = [LGMI() for _ in range(3)]
+    def find_points(x, y):
+        if a: yield (-(b * y + c) / a, y)
+        if b: yield (x, -(a * x + c) / b)
 
-    grid = [
-        [0, 2, 1],
-        [1, 0, 2],
-        [2, 1, 0]
-    ]
+    a, b, c = MII()
+    x1, y1, x2, y2 = MII()
 
-    a -= 1
-    b -= 1
+    ans = abs(x1 - x2) + abs(y1 - y2)
+    for pt1 in find_points(x1, y1):
+        for pt2 in find_points(x2, y2):
+            ans = min(ans, dis((x1, y1), pt1) + dis(pt1, pt2) + dis(pt2, (x2, y2)))
 
-    vis = {}
-    acc1 = [0]
-    acc2 = [0]
-
-    for i in range(k):
-        if a * 3 + b in vis:
-            last_vis = vis[a * 3 + b]
-            cycle = i - last_vis
-            x, y = divmod(k - last_vis, cycle)
-            ans1 = acc1[last_vis] + x * (acc1[i] - acc1[last_vis]) + acc1[last_vis + y] - acc1[last_vis]
-            ans2 = acc2[last_vis] + x * (acc2[i] - acc2[last_vis]) + acc2[last_vis + y] - acc2[last_vis]
-            print(ans1, ans2)
-            exit()
-        
-        acc1.append(acc1[-1])
-        acc2.append(acc2[-1])
-        if grid[a][b] == 1: acc1[-1] += 1
-        elif grid[a][b] == 2: acc2[-1] += 1
-        vis[a * 3 + b] = i
-        a, b = grid1[a][b], grid2[a][b]
-
-    print(acc1[-1], acc2[-1])
+    print(ans)
