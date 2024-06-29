e5af275b423806d7b4b06b98c7d45091d8ae30cd
Yawn-Sean <106463906+Yawn-Sean@users.noreply.github.com>
2024-06-29 19:31:43 +0800

    Update cf1085d_lyongwolf.java

--- daily_problems/2024/06/0627/personal_submission/cf1085d_lyongwolf.java
+++ daily_problems/2024/06/0627/personal_submission/cf1085d_lyongwolf.java
@@ -18,24 +18,4 @@ public class cf1085d_lyongwolf {
         }
         out.println((double) s * 2 / cnt);
     }
-
-
-    
-    static boolean retest = false;
-    static FastReader sc = new FastReader();
-    static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
-    public static void main(String[] args) {
-        if (retest) {int t = sc.nextInt(); while (t-- > 0) solve();} else solve(); out.flush(); out.close();
-    }
-    static class FastReader {
-        BufferedReader r = new BufferedReader(new InputStreamReader(System.in)); 
-        StringTokenizer st;
-        String next() {
-            try {while (st == null || !st.hasMoreTokens()) st = new StringTokenizer(r.readLine()); return st.nextToken();} 
-            catch (Exception e) {return null;}
-        }
-        int nextInt() {return Integer.parseInt(next());}
-        long nextLong() {return Long.parseLong(next());}
-        double nextDouble() {return Double.parseDouble(next());}
-    }
 }
