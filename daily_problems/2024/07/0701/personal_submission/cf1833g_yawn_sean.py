9cdde53c233f6a5dd218e211e51aa2c54b6feba9
Yawn-Sean <1900015431@pku.edu.cn>
2024-07-01 08:32:45 +0800

    20240701 Yawn Sean's submission for CF1833G

--- daily_problems/2024/07/0701/personal_submission/cf1833g_yawn_sean.py
+++ daily_problems/2024/07/0701/personal_submission/cf1833g_yawn_sean.py
@@ -12,34 +12,29 @@ def main():
         path[u].append(i)
         path[v].append(i)
     
-    if n % 3: return -1
-    else:
-        parent = [-1] * n
-        order = []
-        
-        stack = [0]
-        while stack:
-            u = stack.pop()
-            order.append(u)
-            for i in path[u]:
-                v = us[i] ^ vs[i] ^ u
-                if v and parent[v] == -1:
-                    parent[v] = i
-                    stack.append(v)
-        
-        size = [1] * n
-        flg = True
-        ans = []
-        for u in reversed(order):
-            if u > 0:
-                if size[u] > 3: 
-                    flg = False
-                elif size[u] == 3:
-                    ans.append(parent[u])
-                else:
-                    p = us[parent[u]] ^ vs[parent[u]] ^ u
-                    size[p] += size[u]
+    parent = [-1] * n
+    order = []
+    
+    stack = [0]
+    while stack:
+        u = stack.pop()
+        order.append(u)
+        for i in path[u]:
+            v = us[i] ^ vs[i] ^ u
+            if v and parent[v] == -1:
+                parent[v] = i
+                stack.append(v)
+    
+    size = [1] * n
+    ans = []
+    for u in reversed(order):
+        if u > 0:
+            if size[u] == 3:
+                ans.append(parent[u])
+            else:
+                p = us[parent[u]] ^ vs[parent[u]] ^ u
+                size[p] += size[u]
 
-        if flg and size[0] == 3:
-            return ans
-        return -1
+    if size[0] == 3:
+        return ans
+    return -1
