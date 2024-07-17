defa946edefaaed438425d634d04d7d33179ec5e
Yawn-Sean <1900015431@pku.edu.cn>
2024-07-17 08:32:47 +0800

    20240717 Yawn Sean's submission for CF243B

---
+++ daily_problems/2024/07/0717/personal_submission/cf243b_yawn_sean.py
@@ -0,0 +1,68 @@
+# Submission Link: https://codeforces.com/contest/243/submission/270944233
+def main():
+    n, m, h, t = MII()
+
+    us = []
+    vs = []
+    path = [[] for _ in range(n)]
+
+    for i in range(m):
+        u, v = GMI()
+        us.append(u)
+        vs.append(v)
+        path[u].append(i)
+        path[v].append(i)
+
+    used = [0] * n
+
+    def check(u, v):
+        if len(path[u]) <= h or len(path[v]) <= t:
+            return
+        
+        cnt = 0
+        
+        for i in path[u]:
+            if not used[us[i] ^ vs[i] ^ u]:
+                cnt += 1
+            used[us[i] ^ vs[i] ^ u] |= 1
+        
+        for i in path[v]:
+            if not used[us[i] ^ vs[i] ^ v]:
+                cnt += 1
+            used[us[i] ^ vs[i] ^ v] |= 2
+        
+        if cnt >= h + t:
+            v1 = [us[i] ^ vs[i] ^ u for i in path[u] if used[us[i] ^ vs[i] ^ u] == 1]
+            v2 = [us[i] ^ vs[i] ^ v for i in path[v] if used[us[i] ^ vs[i] ^ v] == 2]
+            v3 = [us[i] ^ vs[i] ^ u for i in path[u] if used[us[i] ^ vs[i] ^ u] == 3]
+            chosen_1 = v1[:h]
+            chosen_2 = v2[:t]
+            for x in v3:
+                if len(chosen_1) < h:
+                    chosen_1.append(x)
+                elif len(chosen_2) < t:
+                    chosen_2.append(x)
+            print('YES')
+            print(u + 1, v + 1)
+            print(' '.join(str(i + 1) for i in chosen_1))
+            print(' '.join(str(i + 1) for i in chosen_2))
+            exit()
+        
+        for i in path[u]:
+            used[us[i] ^ vs[i] ^ u] = 0
+        
+        for i in path[v]:
+            used[us[i] ^ vs[i] ^ v] = 0
+
+    for i in range(m):
+        u, v = us[i], vs[i]
+        
+        used[u] = used[v] = -1
+        check(u, v)
+        used[u] = used[v] = 0
+        
+        used[u] = used[v] = -1
+        check(v, u)
+        used[u] = used[v] = 0
+
+    print('NO')
