cf609c846bc1173a67dd9811c9d4be3e23adc9f5
FancyALad <95732059+FancyALad@users.noreply.github.com>
2024-07-17 17:06:01 +0800

    20240717 fancyalad's submission for CF1594D

---
+++ daily_problems/2024/07/0717/personal_submission/cf1594d_fancyalad.py
@@ -0,0 +1,57 @@
+def solve():
+    n, m = map(int, input().split())
+    g = [set() for _ in range(n)]
+    for _ in range(m):
+        x, y, c = input().split()
+        x = int(x) - 1; y = int(y) - 1
+        if c[0] == 'i':
+            g[x].add(~y)
+            g[y].add(~x)
+        else:
+            g[x].add(y)
+            g[y].add(x)
+    
+    flag = True
+    ans = 0
+    def chk():
+        nonlocal flag, ans
+        vis = [-1] * n
+        for i, v in enumerate(vis):
+            if v != -1:
+                continue
+            vis[i] = 0
+            cnt = cnt1 = 0
+            stk = [i]
+            while stk:
+                cnt += 1
+                x = stk.pop()
+                for y in g[x]:
+                    if y >= 0:
+                        if vis[y] == vis[x]:
+                            continue
+                        elif vis[y] == -1:
+                            stk.append(y)
+                            vis[y] = vis[x]
+                        elif vis[y] != vis[x]:
+                            flag = False
+                            return
+                        if vis[y] == 1: cnt1 += 1
+                    else:
+                        y = ~y
+                        if vis[y] == vis[x] ^ 1:
+                            continue
+                        elif vis[y] == -1:
+                            stk.append(y)
+                            vis[y] = vis[x] ^ 1
+                        elif vis[y] == vis[x]:
+                            flag = False
+                            return
+                        if vis[y] == 1: cnt1 += 1
+            ans += max(cnt - cnt1, cnt1)
+
+    chk()
+    print(ans if flag else -1)    
+
+check = int(input())
+for _ in range(check):
+    solve()
