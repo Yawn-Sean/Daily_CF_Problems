f3ef6f6aa8aae1a4768996c0d0e79651bf256341
catchfree1225 <111169191+catchfree1225@users.noreply.github.com>
2024-07-29 22:37:00 +0800

    Update cf1105d_catchfree1225.py

--- daily_problems/2024/07/0729/personal_submission/cf1105d_catchfree1225.py
+++ daily_problems/2024/07/0729/personal_submission/cf1105d_catchfree1225.py
@@ -7,23 +7,22 @@ def solve():
     mat = [list(input()) for _ in range(n)]
     players = [deque() for _ in range(p)]
     ans = [0] * p
-    rest = 0
     for i in range(n):
         for j in range(m):
             if mat[i][j].isdigit():
                 pi = int(mat[i][j]) - 1
                 players[pi].append(i * m + j)
                 ans[pi] += 1
-            elif mat[i][j] == '.':
-                rest += 1
     
+    notes = [str(i) for i in range(1, p + 1)]
     dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-    while rest:
+    while True: # 有可能跑不完，不可用rest
+        flg = False
         for pi in range(p):
             speed = a[pi]
             for _ in range(speed):
                 q = players[pi]
-                if not q: # 關鍵優化
+                if not q: # 關鍵優化1
                     break
                 for _ in range(len(q)):
                     msk = q.popleft()
@@ -35,12 +34,11 @@ def solve():
                         if mat[x][y] != '.':
                             continue
                         ans[pi] += 1
-                        rest -= 1
-                        if rest == 0:
-                            print(*ans)
-                            return
-                        mat[x][y] = str(pi + 1)
+                        mat[x][y] = notes[pi]
                         q.append(x * m + y)
+            else:
+                flg = True # 還能跑, 關鍵優化2
+        if not flg: break
     print(*ans)
                
 for _ in range(1):
