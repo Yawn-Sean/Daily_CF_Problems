4aa671d4e1101e2ac6055755f8983a1d7b9c58da
noob261 <63112861+noob261@users.noreply.github.com>
2024-08-02 14:08:42 +0800

    Update cf888d_InnerWorldExplorer.cpp

--- daily_problems/2024/08/0802/personal_submission/cf888d_InnerWorldExplorer.cpp
+++ daily_problems/2024/08/0802/personal_submission/cf888d_InnerWorldExplorer.cpp
@@ -2,19 +2,18 @@
 #pragma GCC optimize("O3,Ofast,unroll-loops")
 using namespace std;
 
-using i64 = long long;
+using u64 = unsigned long long;
 
-i64 comb(int n, int k) {
-	if (k > n) return 0;
-	if (k * 2 > n) k = n - k;
-	if (k == 0) return 1;
-
-	i64 result = n;
-	for ( int i = 2; i <= k; ++i ) {
-		result *= (n - i + 1);
-		result /= i;
-	}
-	return result;
+u64 comb(u64 n, u64 k) {
+    if (k > n) {
+        return 0;
+    }
+    u64 r = 1;
+    for (u64 d = 1; d <= k; ++d) {
+        r *= n--;
+        r /= d;
+    }
+    return r;
 }
 
 int main()
@@ -22,7 +21,7 @@ int main()
 	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 	int n, k;
 	cin >> n >> k;
-	i64 ans = 1;
+	u64 ans = 1;
 	if (k >= 2) {
 		ans += comb(n, 2);
 	}
