16adf1f28cbe581d6176170fde9fc03ea9face7d
Openmi <782498274@qq.com>
2024-08-05 23:46:37 +0800

    20240805 Update cf1468j_openmi.cpp

--- daily_problems/2024/08/0805/personal_submission/cf1468j_openmi.cpp
+++ daily_problems/2024/08/0805/personal_submission/cf1468j_openmi.cpp
@@ -1,10 +1,36 @@
 void solve() {
+    int n, m, k, mn = inf<int>, ans = 0;
+    cin >> n >> m >> k;
+    vi us(m), vs(m), lims(m);
+    rep (i, 0, m) {
+        int u, v, lim;
+        cin >> u >> v >> lim;
+        u--, v--;
+        us[i] = u;
+        vs[i] = v;
+        lims[i] = lim;
+        chmin(mn, abs(lim - k));
+    }
+    vi idx(m);
+    iota(all(idx), 0);
+    sort(all(idx), [&](int x, int y) {return lims[x] < lims[y];});
+
+    UFS ufs(n, 0);
+    rep (i, 0, m) {
+        if (ufs.merge(us[idx[i]], vs[idx[i]]) ) {
+            ans += max(0, lims[idx[i]] - k);
+        }
+    }
+    cout << (ans ? ans : mn) << nl;
+}
+
+// version2
+void solve() {
     int n, m, k;
     cin >> n >> m >> k;
     vi us(m), vs(m);
     priority_queue<pii, vc<pii>, greater<>> pql, pqg;
-    set<int> s;
-    rep (i, 0, n) s.insert(i);
+    UFS ufs(n, 0);
     rep (i, 0, m) {
         int u, v, lim;
         cin >> u >> v >> lim;
@@ -14,25 +40,21 @@ void solve() {
         if (lim > k) pqg.emplace(lim - k, i);
         if (lim <= k) pql.emplace(k - lim, i);
     }
-    vector<int> vis(n);
-    int ans = pql.empty() ? inf<int> : pql.top().first;
+    int mnl = pql.empty() ? inf<int> : pql.top().first;
     while (!pql.empty()) {
         auto [v, i] = pql.top();
         pql.pop();
-        if (vis[us[i]] and vis[vs[i]]) continue;
-        vis[us[i]] = vis[vs[i]] = 1;
-        s.erase(us[i]);
-        s.erase(vs[i]);
+        ufs.merge(us[i], vs[i]);
     }
-    if (!pqg.empty()) chmin(ans, pqg.top().first);
+    
+    int mng = pqg.empty() ? inf<int> : pqg.top().first;
+    int ans = 0;
 
-    while (s.size()) {
+    while (!pqg.empty()) {
         auto [v, i] = pqg.top(); pqg.pop();
-        if (vis[us[i]] and vis[vs[i]]) continue;
-        ans += v;
-        vis[us[i]] = vis[vs[i]] = 1;
-        s.erase(us[i]);
-        s.erase(vs[i]);
+        if (ufs.merge(us[i], vs[i])) {
+            ans += v;
+        }
     }
-    cout << ans << nl;
+    cout << (ans ? ans : min(mng, mnl)) << nl;
 }
