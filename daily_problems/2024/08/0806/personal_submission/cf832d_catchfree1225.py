8062173b8552d8cfe6d11762f14dd9d466e1188d
catchfree1225 <111169191+catchfree1225@users.noreply.github.com>
2024-08-06 19:45:01 +0800

    Add files via upload

---
+++ daily_problems/2024/08/0806/personal_submission/cf832d_catchfree1225.py
@@ -0,0 +1,11 @@
+import sys
+input = lambda: sys.stdin.readline().strip()
+MII = lambda: map(int, input().split())
+
+def solve():
+    pass
+               
+for _ in range(int(input())):
+    n = int(input())
+    a = list(MII())
+    solve()
