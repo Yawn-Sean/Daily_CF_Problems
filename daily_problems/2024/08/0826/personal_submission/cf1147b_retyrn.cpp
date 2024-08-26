fc79c9c6c4c35e9d42986eddffb3b2cc17e2622b
anthony-zhang <80021042+YinqiaoZhang@users.noreply.github.com>
2024-08-26 23:03:50 +0800

    Update cf1147b_retyrn.cpp

--- daily_problems/2024/08/0826/personal_submission/cf1147b_retyrn.cpp
+++ daily_problems/2024/08/0826/personal_submission/cf1147b_retyrn.cpp
@@ -3,38 +3,36 @@ pii e[maxn];
 
 void solve() {
     cin >> n >> m;
-    set<pii> S;
     for (int i = 0; i < m; i ++) {
         int x, y;
         cin >> x >> y;
         x --, y --;
         if (x > y) swap(x, y);
         e[i].fi = x, e[i].se = y;
-        S.insert(e[i]);
+    }
+    set<pii> S(e, e + m);
+    
+    vector<int> f = {1};
+    for (int i = 2; i <= n / i; i ++) {
+        if (n % i) continue;
+        f.push_back(i);
+        if (n / i != i) f.push_back(n / i);
     }
     
-    auto check = [&](int d, int n) {
+    for (auto d : f) {
+        bool ok = true;
         for (int i = 0; i < m; i ++) {
             int x = (e[i].fi + d) % n;
             int y = (e[i].se + d) % n;
             if (x > y) swap(x, y);
             auto p = mpr(x, y);
-            if (!S.count(p)) return false;
+            if (S.count(p)) {
+                continue;
+            }
+            ok = false;
+            break;
         }
-        return true;
-    };
-    
-    int n1 = n;
-    for (int d = 2; d <= n / d; d ++) {
-        if (n % d) continue;
-        while (n % d == 0) n /= d;
-        if (check(n1 / d, n1)) {
-            cout << "Yes" << endl;
-            return;
-        }
-    }
-    if (n > 1) {
-        if (check(n1 / n, n1)) {
+        if (ok) {
             cout << "Yes" << endl;
             return;
         }
