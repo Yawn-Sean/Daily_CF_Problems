f7c73b2c9b74d9ea5e14ba6620a7f6d2c597c988
Yawn-Sean <1900015431@pku.edu.cn>
2024-08-26 08:46:52 +0800

    20240826 Yawn Sean's submission for CF1147B

---
+++ daily_problems/2024/08/0826/personal_submission/cf1147b_yawn_sean.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+// #pragma GCC optimize("O3,Ofast,unroll-loops")
+// #include <bits/extc++.h>
+// using namespace __gnu_cxx;
+// using namespace __gnu_pbds;
+using namespace std;
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+
+
+    return 0;
+}
