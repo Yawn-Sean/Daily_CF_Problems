cc7f44af026d707fcf265d8fc9e1685d768fa972
Yawn-Sean <1900015431@pku.edu.cn>
2024-08-26 09:03:14 +0800

    20240826 Yawn Sean's submission for CF1147B

--- daily_problems/2024/08/0826/personal_submission/cf1147b_yawn_sean.cpp
+++ daily_problems/2024/08/0826/personal_submission/cf1147b_yawn_sean.cpp
@@ -11,7 +11,40 @@ signed main()
     cin.tie(0);
     cout.tie(0);
 
+    int n, m;
+    cin >> n >> m;
 
+    vector<int> ls(m), rs(m);
+    set<pair<int, int>> vis;
+
+    for (int i = 0; i < m; i ++) {
+        cin >> ls[i] >> rs[i];
+        ls[i] --, rs[i] --;
+        if (ls[i] > rs[i]) swap(ls[i], rs[i]);
+        vis.emplace(ls[i], rs[i]);
+    }
+
+    int tmp = n;
+    for (int i = 2; i <= n; i ++) {
+        if (tmp % i == 0) {
+            while (tmp % i == 0) tmp /= i;
+            int x = n / i;
+            bool flg = true;
+            for (int j = 0; j < m; j ++) {
+                int nl = (ls[j] + x) % n, nr = (rs[j] + x) % n;
+                if (nl > nr) swap(nl, nr);
+                if (vis.find({nl, nr}) == vis.end()) {
+                    flg = false;
+                    break;
+                }
+            }
+            if (flg) {
+                cout << "Yes";
+                return 0;
+            }
+        }
+    }
+    cout << "No";
 
     return 0;
 }
