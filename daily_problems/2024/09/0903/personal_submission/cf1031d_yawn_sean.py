c52a23ccd1e454fb956ae5773e29f8c0b6492f87
Yawn-Sean <1900015431@pku.edu.cn>
2024-09-03 09:35:34 +0800

    20240903 Yawn Sean's submission for CF1031D

--- daily_problems/2024/09/0903/personal_submission/cf1031d_yawn_sean.py
+++ daily_problems/2024/09/0903/personal_submission/cf1031d_yawn_sean.py
@@ -1,148 +1,51 @@
-# Visit my repo: https://github.com/Yawn-Sean/Daily_CF_Problems
-standard_input, packages, output_together = 1, 1, 0
-dfs, hashing, read_from_file = 1, 0, 0
-de = 1
+# Submission link: https://codeforces.com/contest/1031/submission/279399191
+def main():
+    n, k = MII()
+    grid = [I() for _ in range(n)]
 
-if 1:
+    dp = [2 * n] * (n * n)
+    def f(x, y): return x * n + y
 
-    if standard_input:
-        import io, os, sys
-        input = lambda: sys.stdin.readline().strip()
+    dp[f(0, 0)] = 0
 
-        import math
-        inf = math.inf
+    for i in range(n):
+        for j in range(n):
+            if i and dp[f(i-1, j)] < dp[f(i, j)]:
+                dp[f(i, j)] = dp[f(i-1, j)]
+            if j and dp[f(i, j-1)] < dp[f(i, j)]:
+                dp[f(i, j)] = dp[f(i, j-1)]
+            dp[f(i, j)] += 0 if grid[i][j] == 'a' else 1
 
-        def I():
-            return input()
-        
-        def II():
-            return int(input())
+    cur = 0
+    tmp = [f(0, 0)]
+    for i in range(n):
+        for j in range(n):
+            if dp[f(i, j)] <= k:
+                if i + j > cur:
+                    cur = i + j
+                    tmp = [f(i, j)]
+                elif i + j == cur:
+                    tmp.append(f(i, j))
 
-        def MII():
-            return map(int, input().split())
+    ans = ['a' * cur]
+    ans.append('a' if dp[tmp[0]] <= k else grid[0][0])
 
-        def LI():
-            return list(input().split())
+    dirs = [(0, 1), (1, 0)]
 
-        def LII():
-            return list(map(int, input().split()))
+    for _ in range(cur + 1, 2 * n - 1):
+        ntmp = []
+        nc = chr(ord('z') + 1)
+        for msk in tmp:
+            x, y = divmod(msk, n)
+            for dx, dy in dirs:
+                if x + dx < n and y + dy < n:
+                    if grid[x+dx][y+dy] < nc:
+                        nc = grid[x+dx][y+dy]
+                        ntmp = [f(x + dx, y + dy)]
+                    elif grid[x+dx][y+dy] == nc:
+                        if f(x+dx, y+dy) != ntmp[-1]:
+                            ntmp.append(f(x + dx, y + dy))
+        ans.append(nc)
+        tmp = ntmp
 
-        def LFI():
-            return list(map(float, input().split()))
-
-        def GMI():
-            return map(lambda x: int(x) - 1, input().split())
-
-        def LGMI():
-            return list(map(lambda x: int(x) - 1, input().split()))
-
-    if packages:
-        from io import BytesIO, IOBase
-
-        import random
-        import os
-
-        import bisect
-        import typing
-        from collections import Counter, defaultdict, deque
-        from copy import deepcopy
-        from functools import cmp_to_key, lru_cache, reduce
-        from heapq import merge, heapify, heappop, heappush, heappushpop, nlargest, nsmallest
-        from itertools import accumulate, combinations, permutations, count, product
-        from operator import add, iand, ior, itemgetter, mul, xor
-        from string import ascii_lowercase, ascii_uppercase, ascii_letters
-        from typing import *
-        BUFSIZE = 4096
-
-    if output_together:
-        class FastIO(IOBase):
-            newlines = 0
-
-            def __init__(self, file):
-                self._fd = file.fileno()
-                self.buffer = BytesIO()
-                self.writable = "x" in file.mode or "r" not in file.mode
-                self.write = self.buffer.write if self.writable else None
-
-            def read(self):
-                while True:
-                    b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
-                    if not b:
-                        break
-                    ptr = self.buffer.tell()
-                    self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
-                self.newlines = 0
-                return self.buffer.read()
-
-            def readline(self):
-                while self.newlines == 0:
-                    b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
-                    self.newlines = b.count(b"\n") + (not b)
-                    ptr = self.buffer.tell()
-                    self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
-                self.newlines -= 1
-                return self.buffer.readline()
-
-            def flush(self):
-                if self.writable:
-                    os.write(self._fd, self.buffer.getvalue())
-                    self.buffer.truncate(0), self.buffer.seek(0)
-
-        class IOWrapper(IOBase):
-            def __init__(self, file):
-                self.buffer = FastIO(file)
-                self.flush = self.buffer.flush
-                self.writable = self.buffer.writable
-                self.write = lambda s: self.buffer.write(s.encode("ascii"))
-                self.read = lambda: self.buffer.read().decode("ascii")
-                self.readline = lambda: self.buffer.readline().decode("ascii")
-
-        sys.stdout = IOWrapper(sys.stdout)
-
-    if dfs:
-        from types import GeneratorType
-
-        def bootstrap(f, stk=[]):
-            def wrappedfunc(*args, **kwargs):
-                if stk:
-                    return f(*args, **kwargs)
-                else:
-                    to = f(*args, **kwargs)
-                    while True:
-                        if type(to) is GeneratorType:
-                            stk.append(to)
-                            to = next(to)
-                        else:
-                            stk.pop()
-                            if not stk:
-                                break
-                            to = stk[-1].send(to)
-                    return to
-            return wrappedfunc
-
-    if hashing:
-        RANDOM = random.getrandbits(20)
-        class Wrapper(int):
-            def __init__(self, x):
-                int.__init__(x)
-
-            def __hash__(self):
-                return super(Wrapper, self).__hash__() ^ RANDOM
-
-    if read_from_file:
-        file = open("input.txt", "r").readline().strip()[1:-1]
-        fin = open(file, 'r')
-        input = lambda: fin.readline().strip()
-        output_file = open("output.txt", "w")
-        def fprint(*args, **kwargs):
-            print(*args, **kwargs, file=output_file)
-
-    if de:
-        def debug(*args, **kwargs):
-            print('\033[92m', end='')
-            print(*args, **kwargs)
-            print('\033[0m', end='')
-
-    fmax = lambda x, y: x if x > y else y
-    fmin = lambda x, y: x if x < y else y
-
+    print(''.join(ans))
