1e4677beb7ff30d3d05bab622df0f5f076c155ce
mdwang <mengdiwang@users.noreply.github.com>
2024-09-02 17:00:41 -0700

    update

--- daily_problems/2024/09/0902/personal_submission/cf117b_md.cpp
+++ daily_problems/2024/09/0902/personal_submission/cf117b_md.cpp
@@ -1,22 +1,23 @@
 // 0902
 #include <bits/stdc++.h>
+using i64 = long long;
 
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
-    int a, b, mod;
+    i64 a, b, mod;
     std::cin >> a >> b >> mod;
 
-    int s1 = static_cast<int>(1e9) % mod;
-    int rem = 0;
-    for (int i = 0; i <= a && i < mod; ++i) {
-        if (rem > b) {
+    constexpr i64 offset = static_cast<i64>(1e9);
+    i64 s2 = 0;
+    for (i64 i = 0; i <= a && i < mod; ++i) {
+        s2 = (mod - (i * offset) % mod) % mod;
+        if (s2 > b) {
             auto val = std::to_string(i);
             std::cout << "1 " << std::string(9 - ssize(val), '0') << val << '\n';
             return 0;
         }
-        rem = (rem + mod - s1) % mod;
     }
 
     std::cout << "2\n";
