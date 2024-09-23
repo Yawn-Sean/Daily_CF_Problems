a3172c1f90f8c70e632ea8bd8963e8796263b776
dinghc <dinghc7955@163.com>
2024-09-23 21:28:31 +0800

    20240923 submission

--- daily_problems/2024/09/0923/personal_submission/cf1073c_dinghc.cpp
+++ daily_problems/2024/09/0923/personal_submission/cf1073c_dinghc.cpp
@@ -7,30 +7,30 @@ int px[N + 1], py[N + 1];
 int sx[N + 1], sy[N + 1];
 
 int main() {
-	int n;
-	scanf("%d %s", &n, s);
-	int x, y;
-	scanf("%d %d", &x, &y);
-	if (n < abs(x) + abs(y) || abs(x + y) % 2 != n % 2) {
-		printf("-1");
-		return 0;
-	}
-	for (int i = 1; i <= n; i++) {
-		py[i] = py[i - 1];
-		px[i] = px[i - 1];
-		if (s[i - 1] == 'U') py[i] += 1;
-		if (s[i - 1] == 'D') py[i] -= 1;
-		if (s[i - 1] == 'L') px[i] -= 1;
-		if (s[i - 1] == 'R') px[i] += 1;
-	}
-	for (int i = n - 1; i >= 0; i--) {
-		sy[i] = sy[i + 1];
-		sx[i] = sx[i + 1];
-		if (s[i] == 'U') sy[i] += 1;
-		if (s[i] == 'D') sy[i] -= 1;
-		if (s[i] == 'L') sx[i] -= 1;
-		if (s[i] == 'R') sx[i] += 1;
-	}
+    int n;
+    scanf("%d %s", &n, s);
+    int x, y;
+    scanf("%d %d", &x, &y);
+    if (n < abs(x) + abs(y) || abs(x + y) % 2 != n % 2) {
+        printf("-1");
+        return 0;
+    }
+    for (int i = 1; i <= n; i++) {
+        py[i] = py[i - 1];
+        px[i] = px[i - 1];
+        if (s[i - 1] == 'U') py[i] += 1;
+        if (s[i - 1] == 'D') py[i] -= 1;
+        if (s[i - 1] == 'L') px[i] -= 1;
+        if (s[i - 1] == 'R') px[i] += 1;
+    }
+    for (int i = n - 1; i >= 0; i--) {
+        sy[i] = sy[i + 1];
+        sx[i] = sx[i + 1];
+        if (s[i] == 'U') sy[i] += 1;
+        if (s[i] == 'D') sy[i] -= 1;
+        if (s[i] == 'L') sx[i] -= 1;
+        if (s[i] == 'R') sx[i] += 1;
+    }
     auto dist = [&] (int nx, int ny) {
         return abs(nx - x) + abs(ny - y);
     };
