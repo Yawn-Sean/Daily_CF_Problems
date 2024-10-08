8c283595c62e95c3912f9bf202e22d283ee97cd7
Cyril Huang <hccyril@163.com>
2024-10-08 13:01:01 +0800

    20241008_liryc_1

--- daily_problems/2024/10/1008/personal_submission/cf2272d2_liryc.py
+++ daily_problems/2024/10/1008/personal_submission/cf2272d2_liryc.py
@@ -9,48 +9,7 @@ Greedy, Fenwick Tree
 import sys
 input = lambda: sys.stdin.readline().strip()
 
-class FenwickTree:
-    def __init__(self, n):
-        self.n = n
-        self.bit = [0] * n
-        length = (self.n + 1).bit_length() - 1
-        self.powers = [1 << i for i in range(length, -1, -1)]
-        self.total = 0
-
-    def sum(self, r):
-        res = 0
-        while r >= 0:
-            res += self.bit[r]
-            r = (r & r + 1) - 1
-        return res
-
-    def rangesum(self, l, r):
-        return self.sum(r) - self.sum(l - 1)
-
-    def add(self, idx, delta):
-        while idx < self.n:
-            self.bit[idx] += delta
-            idx |= idx + 1
-        self.total += delta
-
-    def lower_bound(self, num):
-        if num <= 0: return -1
-        note, tmp = -1, 0
-        for power in self.powers:
-            if note + power >= self.n or tmp + self.bit[note + power] >= num: continue
-            note += power
-            tmp += self.bit[note]
-        return note + 1
-
-    def upper_bound(self, num):
-        if num > self.total: return self.n
-        note = -1
-        tmp = 0
-        for power in self.powers:
-            if note + power >= self.n or tmp + self.bit[note + power] >= num: continue
-            note += power
-            tmp += self.bit[note]
-        return note
+# class FenwickTree:
     
 def solve():
     n = int(input())
