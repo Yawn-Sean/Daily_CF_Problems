6c111a7b53a4619f85c944f09fa1dcc414c10b18
vic <470614197@qq.com>
2024-10-16 20:42:11 +0800

    20241016 yoonsica's submission

--- daily_problems/2024/10/1016/personal_submission/cf641e_yoonsica.py
+++ daily_problems/2024/10/1016/personal_submission/cf641e_yoonsica.py
@@ -1,58 +1,23 @@
-# tle
-import sys
-from collections import defaultdict
-RI = lambda: int(sys.stdin.readline().strip())
-RF = lambda: float(sys.stdin.readline().strip())
-RS = lambda: sys.stdin.readline().strip()
-RFF = lambda: map(float, sys.stdin.readline().strip().split())
-RII = lambda: map(int, sys.stdin.readline().strip().split())
-RILIST = lambda: list(RII())
-RFLIST = lambda: list(RFF())
-mx = lambda x, y: x if x > y else y
-mn = lambda x, y: y if x > y else x
-class BIT:
-    def __init__(self, n):
-        self.tree = [0] * (n + 1)
-
-    def add(self, i, x):
-        while i < len(self.tree):
-            self.tree[i] += x
-            i += i & -i
-
-    def query(self, i):
-        ans = 0
-        while i:
-            ans += self.tree[i]
-            i -= i & -i
-        return ans
-
+# https://codeforces.com/contest/641/submission/286225067
 n = RI()
-q = []
+q = defaultdict(list)
 _set = set()
 for i in range(n):
-    t, x, v = RII()
-    q.append((v, i, t, x)) # 数字， 顺序， 类型， 时间
-    _set.add(x) # 对时间离散化
-
-m = {e: i for i, e in enumerate(sorted(_set), 1)}
+    tp, t, v = RII()
+    q[v].append((i, tp, t)) # 顺序， 类型， 时间
 
 ans = [-1] * n
-cnt = defaultdict(int)
-# 按操作数和操作先后排序，同一个数字的操作集中处理
-q.sort()
-
-tree = BIT(len(m) + 1)
-ans = [-1]*n
-for i in range(n):
-    v, _, t, x = q[i]
-    if i and v != q[i - 1][0]:
-        tree = BIT(len(m) + 1)
-    if t == 1:
-        tree.add(m[x], 1)
-    elif t == 2:
-        tree.add(m[x], -1)
-    else:
-        ans[_] = tree.query(m[x])
+for v, a in q.items():
+    # 对时间离散化
+    m = {e: i for i, e in enumerate(sorted(set([x[2] for x in a])), 1)}
+    tree = BIT(len(m) + 1)
+    for idx, tp, t in a: # 按顺序处理
+        if tp == 1:
+            tree.add(m[t], 1)
+        elif tp == 2:
+            tree.add(m[t], -1)
+        else:
+            ans[idx] = tree.query(m[t])
 
 print('\n'.join(str(x) for x in ans if x != -1))
     
