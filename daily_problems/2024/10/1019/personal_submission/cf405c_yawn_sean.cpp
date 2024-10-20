7cf2f547640aa9fdd505fa9bda142db0ba71ffb5
Yawn-Sean <1900015431@pku.edu.cn>
2024-10-21 07:44:55 +0800

    Problems of 20241021 updated

--- daily_problems/2024/10/1019/personal_submission/cf405c_yawn_sean.cpp
+++ daily_problems/2024/10/1019/personal_submission/cf405c_yawn_sean.cpp
@@ -3,7 +3,13 @@
 // #include <bits/extc++.h>
 // using namespace __gnu_cxx;
 // using namespace __gnu_pbds;
-using namespace std;
+using namespace std; 
+
+long long quickPow(long long base, long long power, long long mod) {
+    if (power == 0) return 1 % mod;
+    long long cur = quickPow(base, power / 2, mod);
+    return power & 1 ? base * cur % mod * cur % mod : cur * cur % mod; 
+}
 
 signed main()
 {
@@ -11,26 +17,36 @@ signed main()
     cin.tie(0);
     cout.tie(0);
 
-    int n, ans = 0;
-    cin >> n;
+    int mod = 998244353;
+    int M = 4e5;
+    vector<int> facs(M + 1, 1), fac_invs(M + 1, 1);
 
-    for (int i = 0; i < n; i ++) {
-        for (int j = 0; j < n; j ++) {
-            int x;
-            cin >> x;
-            if (i == j) ans ^= x;
-        }
+    for (int i = 1; i <= M; i ++) {
+        facs[i] = 1ll * facs[i - 1] * i % mod;
     }
 
-    int q;
-    cin >> q;
-    
-    while (q --) {
-        int t, x;
-        cin >> t;
-        if (t == 3) cout << ans;
-        else cin >> x, ans ^= 1;
+    fac_invs[M] = quickPow(facs[M], mod - 2, mod);
+    for (int i = M; i >= 1; i --) {
+        fac_invs[i - 1] = 1ll * fac_invs[i] * i % mod;
     }
+
+    auto c = [&] (int x, int y) -> int {
+        if (y < 0 || y > x) return 0;
+        return 1ll * facs[x] * fac_invs[x - y] % mod * fac_invs[y] % mod;
+    };
+
+    int h, w;
+    cin >> h >> w;
+
+    int ans = 0;
+    // 一种颜色
+    ans += 3;
+
+    // 两种颜色
+    ans += 2 * (c(h + w, h) - 2) % mod;
+    if (ans >= mod) ans -= mod;
+
+    cout << ans << '\n';
 
     return 0;
 }
