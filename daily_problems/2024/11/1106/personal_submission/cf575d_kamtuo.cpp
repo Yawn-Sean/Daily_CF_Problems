cd3f064dabc23421f1deaae0252b9b4509b16d68
KaMtuo <75929470+kamtuo@users.noreply.github.com>
2024-11-06 14:53:58 +0800

    20241106 Create cf575d_kamtuo.cpp

---
+++ daily_problems/2024/11/1106/personal_submission/cf575d_kamtuo.cpp
@@ -0,0 +1,28 @@
+/*----------------------------------*/
+/* Author : KaMtuo                  */
+/* Email : kamtuo@qq.com            */
+/* Creation_time : 2024-11-06 14:50 */
+/* Software : Visual Studio Code    */
+/*----------------------------------*/
+
+#ifndef KAMTUO_LEETCODE
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cout << 2000 << "\n";
+    for (int i = 1; i <= 1000; i ++) {
+        cout << i << " 1 " << i << " 2\n"; 
+    }
+    for (int i = 1000; i; i --) {
+        cout << i << " 1 " << i << " 2\n"; 
+    }
+    return 0;
+}
+
+#endif
+
+/*
+
+*/
