3b1cf69440a6527c32507eb2faeceb190b394c02
Yawn-Sean <1900015431@pku.edu.cn>
2024-12-24 08:34:01 +0800

    20241224 Yawn Sean's submission for CF228E

--- daily_problems/2024/12/1224/personal_submission/cf142b_yawn_sean.cpp
+++ daily_problems/2024/12/1224/personal_submission/cf142b_yawn_sean.cpp
@@ -1,40 +1,40 @@
-# Submission link: https://codeforces.com/contest/228/submission/298120972
-def main():
-    n, m = MII()
-    path = [[] for _ in range(n)]
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+// #include "atcoder/convolution"
+// #include "atcoder/dsu"
+// #include "atcoder/fenwicktree"
+// #include "atcoder/lazysegtree"
+// #include "atcoder/math"
+// #include "atcoder/maxflow"
+// #include "atcoder/mincostflow"
+// #include "atcoder/modint"
+// #include "atcoder/scc"
+// #include "atcoder/segtree"
+// #include "atcoder/string"
+// #include "atcoder/twosat"
+// using namespace __gnu_cxx;
+// using namespace __gnu_pbds;
+using namespace std;
+auto rng = mt19937(random_device()());
+auto rngl = mt19937_64(random_device()());
 
-    for _ in range(m):
-        u, v, w = MII()
-        u -= 1
-        v -= 1
-        if w:
-            path[u].append(v)
-            path[v].append(u)
-        else:
-            path[u].append(~v)
-            path[v].append(~u)
+// Let's set a bit and flow!
+// I came, I divide, I conquer!
 
-    cols = [-1] * n
-    for i in range(n):
-        if cols[i] == -1:
-            cols[i] = 0
-            stk = [i]
-            while stk:
-                u = stk.pop()
-                for v in path[u]:
-                    if v >= 0:
-                        if cols[v] == -1:
-                            cols[v] = cols[u]
-                            stk.append(v)
-                        elif cols[v] != cols[u]:
-                            exit(print('Impossible'))
-                    else:
-                        v = ~v
-                        if cols[v] == -1:
-                            cols[v] = cols[u] ^ 1
-                            stk.append(v)
-                        elif cols[v] == cols[u]:
-                            exit(print('Impossible'))
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
 
-    print(sum(cols))
-    print(' '.join(str(i + 1) for i in range(n) if cols[i]))
+    int n, m;
+    cin >> n >> m;
+
+    if (min(n, m) == 1) cout << n * m;
+    else if (min(n, m) == 2) {
+        int v = max(n, m);
+        cout << v / 4 * 4 + min(v % 4, 2) * 2;
+    }
+    else cout << (n * m + 1) / 2;
+
+    return 0;
+}
