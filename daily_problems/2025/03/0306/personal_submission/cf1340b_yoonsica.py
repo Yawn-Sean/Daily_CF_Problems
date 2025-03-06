71bacf3b9d1578e6a4fad8992c53696135c37ff3
vic <470614197@qq.com>
2025-03-06 18:26:03 +0800

    20250306 yoonsica's submission

--- daily_problems/2025/03/0306/personal_submission/cf1340b_yoonsica.py
+++ daily_problems/2025/03/0306/personal_submission/cf1340b_yoonsica.py
@@ -31,37 +31,34 @@ def dfs(s, cur, i, cnt):
 for x in a:
     dfs(x, '', 0, 0)
 
-# print(d['0000000'][7])
-# print(d['0010010'])
 n, k = RII()
 a = []
 for _ in range(n):
     a.append(RS())
 
 f = [[0] * (k + 1) for _ in range(n + 1)]
-f[n][0] = 1
+f[n][k] = 1
 for i in range(n - 1, -1, -1):
     x = a[i]
     for j in range(k + 1):
-        if x in m:
-            f[i][j] = f[i + 1][j]
-        for c in range(1, mn(j, 7) + 1):
-            if f[i + 1][j - c] and (d[x][c] != -inf):
+        for c in range(mn(k - j, 7) + 1):
+            # j + c <= k
+            if f[i + 1][j + c] and (d[x][c] != -inf):
                 f[i][j] = 1
                 break
-
-if f[0][k]:
+if f[0][0]:
     # 贪心从高到低构造
     ans = []
-    for i, x in enumerate(a, 1):
-        t, cnt = -1, 0
-        for j in range(k + 1):
-            if f[i][j]:
-                # 有j - cnt个可以给x
-                if d[x][j] > t:
-                    t, cnt = d[x][j], j
+    j = 0
+    for i, x in enumerate(a):
+        t, c = -1, 0
+        for cnt in range(k - j + 1):
+            # j + cnt <= k
+            if f[i + 1][cnt + j]:
+                if d[x][cnt] > t:
+                    t, c = d[x][cnt], cnt
         ans.append(t)
-        k -= cnt
+        j += c
     print(''.join(map(str, ans)))
 else:
     print(-1)
