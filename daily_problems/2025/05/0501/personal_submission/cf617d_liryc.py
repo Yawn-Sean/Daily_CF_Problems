f89ffc33a0a6af1ab7ee0fed49aa5fb893b8df9c
Cyril Huang <12328307+hccyril@users.noreply.github.com>
2025-05-01 21:10:47 +0800

    Update cf617d_liryc.py

--- daily_problems/2025/05/0501/personal_submission/cf617d_liryc.py
+++ daily_problems/2025/05/0501/personal_submission/cf617d_liryc.py
@@ -1,5 +1,5 @@
 '''
-https://codeforces.com/problemset/submission/617/317950570
+https://codeforces.com/problemset/submission/617/317950994
 '''
 def solve(a: list[list[int]]):
     if a[0][0] == a[1][0] == a[2][0] or a[0][1] == a[1][1] == a[2][1]:
@@ -11,5 +11,5 @@ def solve(a: list[list[int]]):
             if a[j][0] == a[k][0] and not (min(a[j][1], a[k][1]) < a[i][1] < max(a[j][1], a[k][1])):
                 return 2
             if a[j][1] == a[k][1] and not (min(a[j][0], a[k][0]) < a[i][0] < max(a[j][0], a[k][0])):
-                exit(print(2))
+                return 2
     return 3
