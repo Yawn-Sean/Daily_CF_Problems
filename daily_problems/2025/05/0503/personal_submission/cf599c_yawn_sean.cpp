27990c9b619202d990006e6d8333ce3983df6656
Yawn-Sean <1900015431@pku.edu.cn>
2025-05-03 09:19:32 +0800

    20250503 Yawn Sean's submission for CF599C

---
+++ daily_problems/2025/05/0503/personal_submission/cf599c_yawn_sean.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+// #include "atcoder/convolution"
+// #include "atcoder/dsu"
+// #include "atcoder/fenwicktree"
+// #include "atcoder/lazysegtree"
+// #include "atcoder/math"
+// #include "atcoder/maxflow"
+// #include "atcoder/mincostflow"
+// #include "atcoder/modint"
+// #include "atcoder/scc"
+// #include "atcoder/segtree"
+// #include "atcoder/string"
+// #include "atcoder/twosat"
+// using namespace __gnu_cxx;
+// using namespace __gnu_pbds;
+using namespace std;
+auto rng = mt19937(random_device()());
+auto rngl = mt19937_64(random_device()());
+
+// Let's set a bit and flow!
+// I came, I divided, I conquered!
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+    for (auto &x: nums)
+        cin >> x;
+    
+    vector<int> tmp = nums;
+    sort(tmp.begin(), tmp.end());
+
+    int ans = 0;
+    long long cur = 0;
+    for (int i = 0; i < n; i ++) {
+        cur += tmp[i] - nums[i];
+        ans += (cur == 0);
+    }
+
+    cout << ans;
+
+    return 0;
+}
