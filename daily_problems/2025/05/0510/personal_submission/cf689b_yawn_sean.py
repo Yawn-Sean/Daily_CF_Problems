16fdcdf6244d943229406485cc72b2d6d5dba91e
Yawn-Sean <1900015431@pku.edu.cn>
2025-05-10 10:46:30 +0800

    20250510 Yawn Sean's submission for CF689B

--- daily_problems/2025/05/0510/personal_submission/cf689b_yawn_sean.py
+++ daily_problems/2025/05/0510/personal_submission/cf689b_yawn_sean.py
@@ -1,65 +1,23 @@
-#include <bits/stdc++.h>
-#pragma GCC optimize("O3,Ofast,unroll-loops")
-// #include "atcoder/convolution"
-// #include "atcoder/dsu"
-// #include "atcoder/lazysegtree"
-// #include "atcoder/fenwicktree"
-// #include "atcoder/math"
-// #include "atcoder/maxflow"
-// #include "atcoder/mincostflow"
-// #include "atcoder/modint"
-// #include "atcoder/scc"
-// #include "atcoder/segtree"
-// #include "atcoder/string"
-// #include "atcoder/twosat"
-// using namespace __gnu_cxx;
-// using namespace __gnu_pbds;
-using namespace std;
-auto rng = mt19937(random_device()());
-auto rngl = mt19937_64(random_device()());
-
-// Let's set a bit and flow!
-// I came, I divided, I conquered!
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
-    int n;
-    cin >> n;
-
-    vector<int> nums(n);
-    for (auto &x: nums)
-        cin >> x, x --;
+# Submission link: https://codeforces.com/contest/689/submission/318998746
+def main():
+    n = II()
+    nums = LGMI()
     
-    vector<int> dis(n, -1);
-    dis[0] = 0;
-
-    queue<int> q;
-    q.push(0);
-
-    while (!q.empty()) {
-        int u = q.front();
-        q.pop();
-
-        if (u && dis[u - 1] == -1) {
-            dis[u - 1] = dis[u] + 1;
-            q.push(u - 1);
-        }
-
-        if (u + 1 < n && dis[u + 1] == -1) {
-            dis[u + 1] = dis[u] + 1;
-            q.push(u + 1);
-        }
-
-        if (dis[nums[u]] == -1) {
-            dis[nums[u]] = dis[u] + 1;
-            q.push(nums[u]);
-        }
-    }
-
-    for (auto &x: dis) cout << x << ' ';
-
-    return 0;
-}
+    dis = [-1] * n
+    dis[0] = 0
+    
+    que = [0]
+    for u in que:
+        if u and dis[u - 1] == -1:
+            dis[u - 1] = dis[u] + 1
+            que.append(u - 1)
+        
+        if u + 1 < n and dis[u + 1] == -1:
+            dis[u + 1] = dis[u] + 1
+            que.append(u + 1)
+        
+        if dis[nums[u]] == -1:
+            dis[nums[u]] = dis[u] + 1
+            que.append(nums[u])
+    
+    print(' '.join(map(str, dis)))
