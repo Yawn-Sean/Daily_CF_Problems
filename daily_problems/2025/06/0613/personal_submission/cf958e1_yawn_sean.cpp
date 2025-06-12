a50b168061280a4cb9107bab3ebb17f26ad435ce
Yawn-Sean <1900015431@pku.edu.cn>
2025-06-13 06:26:33 +0800

    20250613 Yawn Sean's submission for CF958E1

---
+++ daily_problems/2025/06/0613/personal_submission/cf958e1_yawn_sean.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3,Ofast,unroll-loops")
+// #include "atcoder/all"
+
+using namespace std;
+auto rng = mt19937(random_device()());
+auto rngl = mt19937_64(random_device()());
+
+// Let's set a bit and flow!
+// I came, I divided, I conquered!
+// using namespace __gnu_cxx;
+// using namespace __gnu_pbds;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int r, b;
+    cin >> r >> b;
+    cout << (r == b ? "Yes" : "No");
+
+    return 0;
+}
---
+++ daily_problems/2025/06/0613/personal_submission/cf958e1_yawn_sean.py
@@ -0,0 +1,4 @@
+# Submission link: https://codeforces.com/contest/958/submission/324170225
+def main():
+    r, b = MII()
+    print('Yes' if r == b else 'No')
