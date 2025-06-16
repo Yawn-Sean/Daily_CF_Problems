76ef2741da49f96445231557466e89d53552b523
Maxwell He <105534888+Mkswll@users.noreply.github.com>
2025-06-15 23:08:36 -0400

    Update cf1949c_mkswll.cpp
    Very good time and memory

--- daily_problems/2025/06/0616/personal_submission/cf1949c_mkswll.cpp
+++ daily_problems/2025/06/0616/personal_submission/cf1949c_mkswll.cpp
@@ -6,52 +6,32 @@ int main(){
 	cin.tie(0);
 	int n;
 	cin >> n;
-	vector adj(n + 1, vector <int> ());
+	vector <int> adj(n + 1), deg(n + 1), siz(n + 1, 1);
 	for(int i = 1; i < n; ++i){
 		int u, v;
 		cin >> u >> v;
-		adj[u].push_back(v);
-		adj[v].push_back(u);
+		adj[u] ^= v;
+		adj[v] ^= u;
+		deg[u]++;
+		deg[v]++;
 	}
-	vector <int> cents;
-	vector <int> siz(n + 1);
-	auto dfs = [&](auto&& self, int cur, int fa) -> void {
-		siz[cur] = 1;
-		int mx = 0;
-		for(int to : adj[cur]){
-			if(to == fa) continue;
-			self(self, to, cur);
-			siz[cur] += siz[to];
-			mx = max(mx, siz[to]);
-		}
-		mx = max(mx, n - siz[cur]);
-		if(mx * 2 <= n) cents.push_back(cur);
-	};
-	auto dfs2 = [&](auto&&self, int cur, int fa) -> bool {
-		siz[cur] = 1;
-		vector <int> sizs;
-		for(int to : adj[cur]){
-			if(to == fa) continue;
-			auto res = self(self, to, cur);
-			if(!res) return 0;
-			siz[cur] += siz[to];
-			sizs.push_back(siz[to]);
-		}
-		sort(sizs.begin(), sizs.end());
-		int sm = 1;
-		for(auto i : sizs){
-			if(sm < i) return 0;
-			sm += i;
-		}
-		return 1;
-	};
-	dfs(dfs, 1, 0);
-	for(auto cent : cents){
-		if(dfs2(dfs2, cent, 0)){
-			cout << "YES\n";
+	priority_queue <pair <int, int>, vector <pair <int, int> >, greater <pair <int, int> > > q;
+	for(int i = 1; i <= n; ++i){
+		if(deg[i] == 1) q.push({1, i});
+	}
+	for(int i = 1; i < n; ++i){
+		auto [val, id] = q.top();
+		q.pop();
+		int v = adj[id];
+		if(siz[v] < val){
+			cout << "NO\n";
 			return 0;
 		}
+		--deg[v];
+		adj[v] ^= id;
+		siz[v] += val;
+		if(deg[v] == 1) q.push({siz[v], v});
 	}
-	cout << "NO\n";
+	cout << "YES\n";
 	return 0;
 }
