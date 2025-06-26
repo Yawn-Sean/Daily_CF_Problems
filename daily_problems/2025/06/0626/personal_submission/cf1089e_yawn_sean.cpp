704760291921c1340eeaefe6a5bdcc16eec6c2fe
Yawn-Sean <1900015431@pku.edu.cn>
2025-06-26 08:10:40 +0800

    20250626 Yawn Sean's submission for CF260D

--- daily_problems/2025/06/0626/personal_submission/cf1089e_yawn_sean.cpp
+++ daily_problems/2025/06/0626/personal_submission/cf1089e_yawn_sean.cpp
@@ -19,44 +19,41 @@ int main() {
     int n;
     cin >> n;
 
-    if (n == 2) cout << "a1 h1 h8";
-    else {
-        vector<int> cnt(8, 0);
-        cnt[0] = 1;
-        cnt[7] = 2;
-        n -= 2;
+    vector<int> cnt(8, 0);
+    cnt[0] = 1;
+    cnt[7] = 2;
+    n -= 2;
 
-        for (int i = 0; i < 8; i ++) {
-            int x = min(8 - cnt[i], n);
-            n -= x;
-            cnt[i] += x;
-        }
-
-        int cur = 1;
-
-        for (int i = 0; i < 7; i ++) {
-            if (cnt[i]) {
-                vector<int> tmp = {cur};
-                for (int j = 1; j <= 8; j ++) {
-                    if (j != cur && tmp.size() < cnt[i]) {
-                        tmp.emplace_back(j);
-                    }
-                }
-                if (cnt[i] == 8) swap(tmp[6], tmp[7]);
-                for (auto &x: tmp) cout << (char)('a' + i) << x << ' ';
-                cur = tmp.back();
-            }
-        }
-
-        vector<int> tmp = {cur};
-        for (int j = 1; j <= 8; j ++) {
-            if (j != cur && tmp.size() < cnt[7]) {
-                tmp.emplace_back(j);
-            }
-        }
-        tmp.back() = 8;
-        for (auto &x: tmp) cout << 'h' << x << ' ';
+    for (int i = 0; i < 8; i ++) {
+        int x = min(8 - cnt[i], n);
+        n -= x;
+        cnt[i] += x;
     }
+
+    int cur = 1;
+
+    for (int i = 0; i < 7; i ++) {
+        if (cnt[i]) {
+            vector<int> tmp = {cur};
+            for (int j = 1; j <= 8; j ++) {
+                if (j != cur && tmp.size() < cnt[i]) {
+                    tmp.emplace_back(j);
+                }
+            }
+            if (cnt[i] == 8) swap(tmp[6], tmp[7]);
+            for (auto &x: tmp) cout << (char)('a' + i) << x << ' ';
+            cur = tmp.back();
+        }
+    }
+
+    vector<int> tmp = {cur};
+    for (int j = 1; j <= 8; j ++) {
+        if (j != cur && tmp.size() < cnt[7]) {
+            tmp.emplace_back(j);
+        }
+    }
+    tmp.back() = 8;
+    for (auto &x: tmp) cout << 'h' << x << ' ';
 
     return 0;
 }
