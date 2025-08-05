7cb17e3d4afc05bd7ccd3bd229b77a2a8257a52c
Cyril Huang <hccyril@163.com>
2025-08-05 18:23:11 +0800

    20250805_liryc

--- daily_problems/2025/08/0805/personal_submission/cf1891d_liryc.py
+++ daily_problems/2025/08/0805/personal_submission/cf1891d_liryc.py
@@ -1,26 +1,25 @@
 '''
-1891D
-2025/08/05 Y2
-1900
-math
+https://codeforces.com/problemset/submission/1891/332485022
 '''
 # enumerations
-from sys import stdin
-input = lambda: stdin.readline().strip()
-pmax = lambda x, y: y if y > x else x
-pmin = lambda x, y: y if y < x else x
-# pmin = lambda x, y: y if x < 0 or y >= 0 and y < x else x
-
-def init():
-    l, r = map(int, input().split())
-    return l, r
-
 def solve(l: int, r: int) -> int:
+    MOD = 1000000007
     f = l.bit_length() - 1
-    return -1
-
-if __name__ == '__main__':
-    for _ in range(int(input())):
-        args = init()
-        ans = solve(*args)
-        print(ans)
+    n = 1 << f + 1
+    g, m = 0, f
+    while m <= l:
+        g, m = g + 1, m * f
+    ans = 0
+    while True:
+        if r < n and r < m:
+            ans = (ans + g * (r - l + 1)) % MOD
+            return ans
+        if n <= m:
+            ans = (ans + g * (n - l)) % MOD
+            l, f, n = n, f + 1, n << 1
+            g, m = 0, f
+            while m <= l:
+                g, m = g + 1, m * f    
+        else:
+            ans = (ans + g * (m - l)) % MOD
+            l, g, m = m, g + 1, m * f        
