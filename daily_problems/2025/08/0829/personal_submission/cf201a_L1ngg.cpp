9bcde70f2d8d65293f4a02ae44719dc9902fb461
L1ngg <2157878170@qq.com>
2025-08-29 21:33:50 +0800

    Add files via upload

---
+++ daily_problems/2025/08/0829/personal_submission/cf201a_L1ngg.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define ranges std::ranges
+#define views std::views
+
+using u32 = unsigned;
+using i64 = long long;
+using u64 = unsigned long long;
+
+using pii = std::pair<int, int>;
+using a3 = std::array<int, 3>;
+using a4 = std::array<int, 4>;
+
+const int dx[8] = { -1, 0, 1, 0, -1, -1, 1, 1 }, dy[8] = { 0, 1, 0, -1, -1, 1, -1, 1 };
+const int N = 1e6;
+const int MAXN = 1e6 + 10;
+const int inf = 1e9;
+const i64 INF = 1e18;
+const int mod = 1e9 + 7;
+
+void solve()
+{
+    int x;  std::cin >> x;
+    int n = 1;
+    while ((n * n + 1) / 2 < x) n += 2;
+    if (x == 3)  std::cout << "5";
+    else std::cout << n;
+}
+
+signed main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    int t = 1;
+    //std::cin >> t;
+    while (t--) {
+        solve();
+        std::cout << '\n';
+    }
+}
