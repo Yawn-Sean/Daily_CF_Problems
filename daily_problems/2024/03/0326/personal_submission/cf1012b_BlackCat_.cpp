2ae82a0988c0f04228c1531a73610b1fd5916adb
BlackCat <145641200+BlackCat-cat@users.noreply.github.com>
2025-09-13 14:57:07 +0800

    Update cf1012b_BlackCat_.cpp

--- daily_problems/2024/03/0326/personal_submission/cf1012b_BlackCat_.cpp
+++ daily_problems/2024/03/0326/personal_submission/cf1012b_BlackCat_.cpp
@@ -5,50 +5,44 @@ using i64 =long long;
  *    2023-08-04: https://ac.nowcoder.com/acm/contest/view-submission?submissionId=63239142
 **/
 struct DSU {
-    std::vector<int> f, siz;
-    
+    std::vector<int> f;
+
     DSU() {}
     DSU(int n) {
         init(n);
     }
-    
+
     void init(int n) {
         f.resize(n);
         std::iota(f.begin(), f.end(), 0);
-        siz.assign(n, 1);
     }
-    
+
     int find(int x) {
         while (x != f[x]) {
             x = f[x] = f[f[x]];
         }
         return x;
     }
-    
+
     bool same(int x, int y) {
         return find(x) == find(y);
     }
-    
+
     bool merge(int x, int y) {
         x = find(x);
         y = find(y);
         if (x == y) {
             return false;
         }
-        siz[x] += siz[y];
         f[y] = x;
         return true;
-    }
-    
-    int size(int x) {
-        return siz[find(x)];
     }
 };
 void solve(){
     int n,m,q;
     std::cin>>n>>m>>q;
     i64 ans=1ll*n+m-1;
-    DSU dsu(n*m+10);
+    DSU dsu(n+m+10);
     while(q--){
         int x,y;
         std::cin>>x>>y;
