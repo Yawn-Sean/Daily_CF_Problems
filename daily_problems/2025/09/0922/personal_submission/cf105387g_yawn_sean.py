85abcf33670fe5d7e9ec866525f166beee9375ee
Yawn-Sean <1900015431@pku.edu.cn>
2025-09-22 00:25:00 +0800

    20250921 Yawn Sean's submission for CF105387G

---
+++ daily_problems/2025/09/0922/personal_submission/cf105387g_yawn_sean.py
@@ -0,0 +1,35 @@
+# Submission link: https://codeforces.com/gym/105387/submission/339732583
+def main():
+    n, kr, kg, kb = MII()
+    
+    dpr = [0] * (n + 1)
+    dpg = [0] * (n + 1)
+    dpb = [0] * (n + 1)
+    
+    dpr[1] = 1
+    dpg[1] = 1
+    dpb[1] = 1
+    
+    mod = 10 ** 9 + 7
+    
+    for i in range(2, n + 1):
+        total = dpr[i - 1] + dpg[i - 1] + dpb[i - 1]
+        
+        dpr[i] = total
+        if i > kr + 1: dpr[i] -= dpg[i - kr - 1] + dpb[i - kr - 1]
+        elif i == kr + 1: dpr[i] -= 1
+        
+        dpg[i] = total
+        if i > kg + 1: dpg[i] -= dpr[i - kg - 1] + dpb[i - kg - 1]
+        elif i == kg + 1: dpg[i] -= 1
+        
+        dpb[i] = total
+        if i > kb + 1: dpb[i] -= dpr[i - kb - 1] + dpg[i - kb - 1]
+        elif i == kb + 1: dpb[i] -= 1
+        
+        dpr[i] %= mod
+        dpg[i] %= mod
+        dpb[i] %= mod
+        
+    
+    print((dpr[n] + dpg[n] + dpb[n]) % mod)
