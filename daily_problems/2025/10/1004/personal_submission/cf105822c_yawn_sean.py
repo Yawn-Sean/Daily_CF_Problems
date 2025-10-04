ba511d05489be3c340ca4d6951139370dd2ecc3e
Yawn-Sean <1900015431@pku.edu.cn>
2025-10-04 08:05:23 +0800

    20251004 Yawn Sean's submission for CF105822C

--- daily_problems/2025/10/1004/personal_submission/cf105822c_yawn_sean.py
+++ daily_problems/2025/10/1004/personal_submission/cf105822c_yawn_sean.py
@@ -1,50 +1,27 @@
-# Submission link: https://codeforces.com/gym/105822/submission/341615762
+# Submission link: https://codeforces.com/gym/105822/submission/341614854
 def main():
     t = II()
     outs = []
     
     for _ in range(t):
-        n, m, k = MII()
-        visits = LGMI()
-        
-        path = [[] for _ in range(n)]
-        
-        for _ in range(m):
-            u, v = GMI()
-            path[u].append(v)
-            path[v].append(u)
-        
-        cur = 0
-        for x in visits:
-            if x != cur:
-                path[x].append(cur)
-                path[cur].append(x)
-                cur = x
-        
-        dis = [-1] * n
-        dis[0] = 0
-        
-        que = [0]
-        
-        for u in que:
-            for v in path[u]:
-                if dis[v] == -1:
-                    dis[v] = dis[u] + 1
-                    que.append(v)
-        
-        flg = True
-        for i in range(1, k):
-            if dis[visits[i]] <= dis[visits[i - 1]]:
-                flg = False
-        
-        if flg:
-            outs.append(str(k if visits[0] else k - 1))
-            cur = 0
-            for x in visits:
-                if x != cur:
-                    outs.append(f'{cur + 1} {x + 1}')
-                    cur = x
+        n = II()
+        s = I()
+        if s.count('E') % 2: outs.append('NO')
         else:
-            outs.append('-1')
+            outs.append('YES')
+            pt1 = 1
+            pt2 = 2
+            
+            for i in range(n):
+                if s[i] == 'O':
+                    outs.append(f'{pt1} {pt2}')
+                    pt1 += 2
+                    pt2 += 2
+                elif pt1 < pt2:
+                    outs.append(f'{pt1} {pt1 + 2}')
+                    pt1 += 4
+                else:
+                    outs.append(f'{pt2} {pt2 + 2}')
+                    pt2 += 4
     
     print('\n'.join(outs))
