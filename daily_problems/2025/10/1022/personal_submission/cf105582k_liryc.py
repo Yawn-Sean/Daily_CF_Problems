3d7bf7a3f36ac7363a1fbaf821f6e8db945849b3
Cyril Huang <hccyril@163.com>
2025-10-26 00:39:17 +0800

    cf105582k_liryc.py

--- daily_problems/2025/10/1022/personal_submission/cf105582k_liryc.py
+++ daily_problems/2025/10/1022/personal_submission/cf105582k_liryc.py
@@ -1,35 +1,22 @@
 '''
-https://codeforces.com/gym/105582/my#
+https://codeforces.com/gym/105582/submission/345780752
 '''
-# ref
+# scale of 3-4-5 triangle
+# 奇数的想不到，从样例1那里照搬的
 def solve(n: int) -> list[list[int]]:
-    pa = []
+    if n == 3:
+        return [[0, 0], [4, 3], [-20, 21]]
+    elif n == 4:
+        return [[0, 3], [4, 0], [0, -3], [-4, 0]]
+
+    pa = [[0, 0]]
+    x, k = 0, 0
+    for _ in range(n // 2 - 1):
+        pa.append([x + 9, 12])
+        pa.append([x + 25, 0])
+        x += 25
+        k += 1
+    pa.append([16 * k, -12 * k])
     if n & 1:
-        pa.append([0, 0])
-        
-        x, y = 0, 0
-        for i in range(n // 2 - 1):
-            y += 25
-            pa.append([x, y])
-            x += 25
-            pa.append([x, y])
-        
-        x = max(x, 25)
-        pa.append([x, 0])
-        pa.append([x // 25 * 9, -x // 25 * 12])
-    
-    else:
-        pa.append([0, 0])
-        
-        x, y = 0, 0
-        for i in range(n // 2 - 1):
-            y += 25
-            pa.append([x, y])
-            x += 25
-            pa.append([x, y])
-        
-        pa.append([x, 0])
-    
-    dx, dy = 5, 12
-    
-    return [[x * dx + y * dy, x * (-dy) + y * dx] for x, y in pa]
+        pa.append([-80 * k, -84 * k])
+    return pa
