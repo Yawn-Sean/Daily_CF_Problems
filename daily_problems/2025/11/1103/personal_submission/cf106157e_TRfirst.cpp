85026cfb33bc77d41622417a28d944a3079802ad
TRfirst <15011027707@163.com>
2025-11-03 19:24:02 +0800

    20251103

--- daily_problems/2025/11/1103/personal_submission/cf106157e_TRfirst.cpp
+++ daily_problems/2025/11/1103/personal_submission/cf106157e_TRfirst.cpp
@@ -1,18 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define endl '\n'
-// #define int long long
-typedef long long ll;
-typedef unsigned long long ull;
-using i128 = __int128_t;
-const int N = 0x7fffffff;
-const int mod = 998244353;
-const int X4[4] = {1, -1, 0, 0}, Y4[4] = {0, 0, 1, -1};
-const int X8[8] = {0, 1, 1, 1, 0, -1, -1, -1}, Y8[8] = {1, 1, 0, -1, -1, -1, 0, 1};
-const int Xh[8] = {1, 2, 2, 1, -1, -2, -2, -1};
-const int Yh[8] = {2, 1, -1, -2, -2, -1, 1, 2};
 const double pi = acos(-1);
-mt19937 rng(time(nullptr));
 
 
 
