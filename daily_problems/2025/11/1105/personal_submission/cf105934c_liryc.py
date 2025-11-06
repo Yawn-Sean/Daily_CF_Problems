86c6b67f87981f157d8274127eeac52e452c8dfb
Cyril Huang <hccyril@163.com>
2025-11-06 10:37:25 +0800

    20251106_liryc

--- daily_problems/2025/11/1105/personal_submission/cf105934c_liryc.py
+++ daily_problems/2025/11/1105/personal_submission/cf105934c_liryc.py
@@ -1,33 +1,25 @@
 '''
-GYM105934-C
+https://codeforces.com/gym/105934/submission/347550696
 '''
-def solve(n: int, a: list[int]) -> int: 
-    ans = 10 ** 18
-    
-    r = 0
-    c = a[0]
-    
-    for i in range(1, n):
-        x = a[i]
-        if i & 1:
-            c = max(c + 1, x)
-            r += c - x
+# greedy
+from sys import stdin
+input = lambda: stdin.readline().strip()
+
+# s: 1 or -1, means compare bigger or smaller
+def solve(n: int, a: list[int], s: int) -> int: 
+    p = (a[0] * s - 1) * s
+    ans = 0
+    for x in a:
+        if x * s > p * s:
+            p = x
         else:
-            c = min(c - 1, x)
-            r += x - c
-    
-    ans = min(ans, r)
-    
-    r = 0
-    c = a[0]
-    
-    for i in range(1, n):
-        if not (i & 1):
-            c = max(c + 1, x)
-            r += c - x
-        else:
-            c = min(c - 1, x)
-            r += x - c
-    
-    ans = min(ans, r)
+            p = (p * s + 1) * s
+            ans += abs(p - x)
+        s = -s
     return ans
+
+if __name__ == '__main__':
+    n = int(input())
+    a = list(map(int, input().split()))
+    print(min(solve(n, a, 1), solve(n, a, -1)))
+
