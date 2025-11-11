797de050b060c05cd7ac86a9afbbc963910d419e
Bone-Light <155127878+Bone-Light@users.noreply.github.com>
2025-11-12 07:37:07 +0800

    20251111 cf106177f_Bonelight.cpp

---
+++ daily_problems/2025/11/1111/personal_submission/cf106177f_Bonelight.cpp
@@ -0,0 +1,61 @@
+/*
+    Bonelight (=v=)
+    20250111: 我是一个史官 
+    == 拿着笔，想写点东西，以为是武器，能声张 正！E！ == 
+    UID: cf1912d 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long
+#define pii pair<int,int>
+#define tiii tuple<int,int,int>
+#define INF 2e18
+
+#define VC vector<int>
+#define MAT vector<vector<int>>
+#define VCii vector<pii>
+#define MATii vector<vector<pii>>
+
+#define For(_i,_l,_r) for(int _i = _l; _i <= _r; _i ++)
+#define Rof(_i,_r,_l) for(int _i = _r; _i >= _l; _i --)
+#define all(_a) begin(_a), end(_a)
+#define all1(_a) begin(_a)+1, end(_a)
+
+#define x first
+#define y second
+#define ls (o << 1)
+#define rs ((o << 1) + 1)
+#define ULL unsigned long long
+
+void solve(){
+    int n; cin >> n;
+    VC nums(n); 
+    for(auto &i:nums) cin >> i;
+    MAT mp(2, VC(2*n + 9));
+    
+    int cnt = n + 3, msk = 0, res = 0;
+    
+    mp[msk][cnt] ++;
+    for(auto i:nums){
+        
+        if(i & 1) msk ^= 1, cnt --;
+        else cnt ++;
+        
+        res += mp[msk][cnt - 1] + mp[msk^1][cnt + 1]; 
+        mp[msk][cnt] ++;
+    }
+    cout << res << endl;  
+}
+signed main(){
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("in.txt","rt",stdin);
+    freopen("out.txt","wt",stdout);
+#endif
+    int T = 1; cin >> T;
+    while(T --) solve();
+    return 0;
+}
