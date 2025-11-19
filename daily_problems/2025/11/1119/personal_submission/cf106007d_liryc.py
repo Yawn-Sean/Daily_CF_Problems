7a5cb60e2c7251e1a38113950b1123f0cf55b616
Cyril Huang <hccyril@163.com>
2025-11-19 16:13:34 +0800

    20251119_liryc(improve)

--- daily_problems/2025/11/1119/personal_submission/cf106007d_liryc.py
+++ daily_problems/2025/11/1119/personal_submission/cf106007d_liryc.py
@@ -1,10 +1,8 @@
 '''
-https://codeforces.com/gym/106007/submission/349646899
+https://codeforces.com/gym/106007/submission/349680751
 '''
 def solve(n: int, a: list[list[int]]) -> list[str]:
-    pa = [-2] * n
-    pa[0] = -1
-    
+    pa = [-1] * n
     qu = deque()
     qu.append(0)
     stk = []
@@ -12,12 +10,11 @@ def solve(n: int, a: list[list[int]]) -> list[str]:
         u = qu.popleft()
         if u:
             stk.append(u)
-        for v in range(n):
-            if a[u][v] and pa[v] == -2:
+        for v in range(1, n):
+            if a[u][v] and pa[v] < 0:
                 pa[v] = u
                 qu.append(v)
-    
-    if len(qu) == n:
+    if len(stk) == n - 1:
         ans = []
         for i in stk[::-1]:
             ans.append(f'{pa[i] + 1} {i + 1}')
