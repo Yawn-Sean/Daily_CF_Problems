127a086f12a196653fc1d3d0ba2feb25c27e391f
tsreaper <tsreaper96@gmail.com>
2025-11-29 20:24:08 +0800

    fix

--- daily_problems/2025/11/1113/personal_submission/cf104671f_tsreaper.cpp
+++ daily_problems/2025/11/1113/personal_submission/cf104671f_tsreaper.cpp
@@ -3,18 +3,22 @@
 #define MAXP 30
 using namespace std;
 
-int n, K, q, f[MAXN + 5][MAXP];
+int n, K, q, f[MAXN + 5][MAXP], g[MAXN + 5];
 
 int main() {
     scanf("%d%d%d", &n, &K, &q);
     for (int i = 1; i <= n; i++) {
         int x; scanf("%d", &x);
         for (int p = 0; p < MAXP; p++) f[i][p] = f[i - 1][p];
-        if ((x & K) == K) for (int p = 0; p < MAXP; p++) if (x >> p & 1 ^ 1) f[i][p]++;
+        g[i] = g[i - 1];
+        if ((x & K) == K) {
+            for (int p = 0; p < MAXP; p++) if (x >> p & 1 ^ 1) f[i][p]++;
+            g[i]++;
+        }
     }
     while (q--) {
         int l, r; scanf("%d%d", &l, &r);
-        bool ok = true;
+        bool ok = (g[r] - g[l - 1] > 0);
         for (int p = 0; p < MAXP; p++) if (K >> p & 1 ^ 1) {
             if (f[r][p] - f[l - 1][p] == 0) { ok = false; break; }
         }
