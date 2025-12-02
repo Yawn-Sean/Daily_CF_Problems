39d2a4cdf65e3b3ba5fa07264d8533801028e283
gxylengli <96274730+gxylengli@users.noreply.github.com>
2025-12-02 10:13:40 +0800

    20251201
    20251201

---
+++ daily_problems/2025/12/1201/personal_submission/cf105316a_lengli.cpp
@@ -0,0 +1,117 @@
+/*
+lengli_QAQ
+Hope there are no bugs!!!
+*/
+#include <bits/stdc++.h>
+#define fastio std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0)
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define i64 long long
+
+template <unsigned M_> struct ModInt {
+  static constexpr unsigned M = M_;
+  unsigned x;
+  constexpr ModInt() : x(0U) {}
+  constexpr ModInt(unsigned x_) : x(x_ % M) {}
+  constexpr ModInt(unsigned long long x_) : x(x_ % M) {}
+  constexpr ModInt(int x_) : x(((x_ %= static_cast<int>(M)) < 0) ? (x_ + static_cast<int>(M)) : x_) {}
+  constexpr ModInt(long long x_) : x(((x_ %= static_cast<long long>(M)) < 0) ? (x_ + static_cast<long long>(M)) : x_) {}
+  ModInt &operator+=(const ModInt &a) { x = ((x += a.x) >= M) ? (x - M) : x; return *this; }
+  ModInt &operator-=(const ModInt &a) { x = ((x -= a.x) >= M) ? (x + M) : x; return *this; }
+  ModInt &operator*=(const ModInt &a) { x = (static_cast<unsigned long long>(x) * a.x) % M; return *this; }
+  ModInt &operator/=(const ModInt &a) { return (*this *= a.inv()); }
+  ModInt pow(long long e) const {
+    if (e < 0) return inv().pow(-e);
+    ModInt a = *this, b = 1U; for (; e; e >>= 1) { if (e & 1) b *= a; a *= a; } return b;
+  }
+  ModInt inv() const {
+    unsigned a = M, b = x; int y = 0, z = 1;
+    for (; b; ) { const unsigned q = a / b; const unsigned c = a - q * b; a = b; b = c; const int w = y - static_cast<int>(q) * z; y = z; z = w; }
+    assert(a == 1U); return ModInt(y);
+  }
+  ModInt operator+() const { return *this; }
+  ModInt operator-() const { ModInt a; a.x = x ? (M - x) : 0U; return a; }
+  ModInt operator+(const ModInt &a) const { return (ModInt(*this) += a); }
+  ModInt operator-(const ModInt &a) const { return (ModInt(*this) -= a); }
+  ModInt operator*(const ModInt &a) const { return (ModInt(*this) *= a); }
+  ModInt operator/(const ModInt &a) const { return (ModInt(*this) /= a); }
+  template <class T> friend ModInt operator+(T a, const ModInt &b) { return (ModInt(a) += b); }
+  template <class T> friend ModInt operator-(T a, const ModInt &b) { return (ModInt(a) -= b); }
+  template <class T> friend ModInt operator*(T a, const ModInt &b) { return (ModInt(a) *= b); }
+  template <class T> friend ModInt operator/(T a, const ModInt &b) { return (ModInt(a) /= b); }
+  explicit operator bool() const { return x; }
+  bool operator==(const ModInt &a) const { return (x == a.x); }
+  bool operator!=(const ModInt &a) const { return (x != a.x); }
+  bool operator<(const ModInt &a) const { return (x < a.x); }
+  bool operator>(const ModInt &a) const { return (x > a.x); }
+  bool operator<=(const ModInt &a) const { return (x <= a.x); }
+  bool operator>=(const ModInt &a) const { return (x >= a.x); }
+  friend std::ostream &operator<<(std::ostream &os, const ModInt &a) { return os << a.x; }
+  friend std::istream &operator>>(std::istream &is, ModInt &a) {int v;is >> v;a = ModInt(v);return is;}
+};
+constexpr unsigned MO = 1000000007;
+using Mint = ModInt<MO>;
+
+struct BIT{
+    int mn;
+    std::vector<Mint> a;
+    BIT(){};
+    BIT(int n){
+        a.clear(),a.resize(n+2);
+        mn=n;
+    };
+    int lowbit(int x){return x&-x;}
+    void add(int x,Mint c) {for(int i=x;i<=mn;i+=lowbit(i)) a[i]+=c;}
+    Mint sum(int x){
+        Mint res=0;
+        for(int i=x;i;i-=lowbit(i)) res+=a[i];
+        return res;
+    }
+};
+
+void solve(){
+    int n;
+    std::cin>>n;
+    std::vector<int> a(n+2);
+    std::vector<i64> pre(n+2);
+    for(int i=1;i<=n;i++){
+        std::cin>>a[i];
+        pre[i]=pre[i-1]+a[i];
+    }
+    i64 sum=pre[n];
+
+    for(int i=1;i<=n;i++){
+        if(a[i]*2>=sum){
+            std::cout<<0<<"\n";
+            return;
+        }
+    }
+
+    std::vector<Mint> f(n+2);
+    f[1]=1;
+    BIT tr(n+2);
+    tr.add(1,f[1]);
+    for(int i=2;i<=n;i++){
+        int l=0,r=i;
+        while(l<r){
+            int mid=(l+r)>>1;
+            if((pre[i]-pre[mid])*2<sum) r=mid;
+            else l=mid+1; 
+        }
+        r++;
+        if(r<=1) f[i]=tr.sum(i)+1;
+        else f[i]=tr.sum(i)-tr.sum(r-2);
+        tr.add(i,f[i]);
+    }
+    std::cout<<f[n]<<"\n";
+}
+
+signed main(){
+    fastio;
+    
+    int T;
+    T=1;
+    while(T--) solve();
+    
+    return 0;
+}
