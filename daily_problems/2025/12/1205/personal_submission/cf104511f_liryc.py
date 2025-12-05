4e2c11f4d200cc371a8e1227876dbecef04d51ea
hccyril <12328307+hccyril@users.noreply.github.com>
2025-12-05 18:26:58 +0800

    104511f_liryc

--- daily_problems/2025/12/1205/personal_submission/cf104511f_liryc.py
+++ daily_problems/2025/12/1205/personal_submission/cf104511f_liryc.py
@@ -1,30 +1,23 @@
 '''
-GYM104511F
-2025/12/5 Y1
-1800
-dijkstra
+https://codeforces.com/gym/104511/submission/351984145
 '''
-# dijkstra
-import sys
-input = lambda: sys.stdin.readline().strip()
-
-def init():
-    n, m, v = map(int, input().split())
-    s = input()
-    trades = [[0, 0, 0, 0, 0, 0] for _ in range(m)]
-    for i in range(m):
-        a, x, b, y, c, z = map(int, input().split())
-        trades[i][:] = (a, x, b, y, c, z)
-    return n, m, v, s, trades
-
 def solve(n: int, m: int, v: int, s: str, trades: list[list[int]]) -> float:
-    sm = int("0b" + s[::-1], 2)
-    def hs(i: int) -> bool:
-        pass
-    return n, s
-
-if __name__ == '__main__':
-    for _ in range(int(input())):
-        args = init()
-        ans = solve(*args)
-        print(ans)
+    ta = []
+    for a, x, b, y, c, z in trades:
+        x -= 1
+        y -= 1
+        z -= 1
+        ta.append(a, x, b, y, c, z)
+    
+    ca = [-1] * n
+    for i in range(n):
+        if s[i] == '1':
+            ca[i] = 1
+    
+    for _ in range(n):
+        for a, x, b, y, c, z in ta:
+            if ca[x] != -1 and ca[y] != -1:
+                if ca[z] == -1: ca[z] = (a * ca[x] + b * ca[y])
+                else: ca[z] = min(ca[z], (a * ca[x] + b * ca[y]))
+    
+    return v / ca[0] if ca[0] != -1 else 0
