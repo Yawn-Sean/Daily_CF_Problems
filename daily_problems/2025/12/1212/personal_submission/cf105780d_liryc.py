30bb6f4b0045015dfaffb074828f459b448f6540
Cyril Huang <hccyril@163.com>
2025-12-12 21:44:07 +0800

    20251212_liryc

--- daily_problems/2025/12/1212/personal_submission/cf105780d_liryc.py
+++ daily_problems/2025/12/1212/personal_submission/cf105780d_liryc.py
@@ -1,35 +1,19 @@
 '''
-2025/12/12 Y1
+https://codeforces.com/gym/105780/submission/353199808
 '''
-
-from sys import stdin
-input = lambda: stdin.readline().strip()
-
-def init():
-    n = int(input())
-    a = [[0, 0] for _ in range(n)]
+# 根据点积公式，对于向量(a, b)，与之垂直又长度相等的只有(b, -a)和(-b, a)
+def solve(n: int, pa: list[list[int]]) -> int:
+    hs = set()
     for i in range(n):
-        a[i][:] = map(int, input().split())
-    return n, a
-
-def solve(n: int, a: list[list[int]]) -> int:
-    hs = set(a)
-    
+        x, y = pa[i]
+        x, y = x << 1, y << 1
+        pa[i][:] = x, y
+        hs.add((x, y))
     ans = 0
-    for i in range(n - 1):
-        for j in range(i + 1, n):
-            x1, y1 = a[i]
-            x2, y2 = a[j]
-            dx = x2 - x1
-            dy = y2 - y1
-            if (x1 + dx - dy, y1 + dx + dy) in hs and (x1 - dy, y1 + dx) in hs:
-                ans += 1
-            if (x1 + dy, y1 - dx) in hs and (x1 + dx + dy, y1 + dy - dx) in hs:
-                ans += 1
-    return ans >> 2    
-
-if __name__ == '__main__':
-    args = init()
-    ans = solve(*args)
-    print(ans)
-
+    for (x1, y1), (x2, y2) in combinations(pa, 2):
+        x, y = x1 + x2 >> 1, y1 + y2 >> 1
+        a, b = x1 - x, y1 - y
+        x3, y3, x4, y4 = x - b, y + a, x + b, y - a
+        if (x3, y3) in hs and (x4, y4) in hs:
+            ans += 1
+    return ans >> 1
