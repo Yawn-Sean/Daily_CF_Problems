64ec8f428480dccb480ada571552f237c545dfd4
koishi2100 <73474070+koishi2100@users.noreply.github.com>
2025-12-20 13:15:38 +0800

    20251220 Koishii's submission for CF105319D
    Implement solution for the Koishii problem using bit manipulation.

---
+++ daily_problems/2025/12/1220/personal_submission/cf105319d_Koishii.py
@@ -0,0 +1,34 @@
+n = int(input())
+nums = list(map(int, input().split()))
+
+l, r = 0, 0
+ans = 0
+
+c0 = [0] * 30
+c1 = [0] * 30
+
+while l < n:
+    while r + 1 < n:
+        v = (nums[r] ^ nums[r + 1]).bit_length() - 1
+        if v >= 0:
+            if nums[r] >> v & 1:
+                if c0[v]:
+                    break
+                c1[v] += 1
+            else:
+                if c1[v]:
+                    break
+                c0[v] += 1
+        r += 1
+
+    ans += r - l + 1
+    if l + 1 < n:
+        v = (nums[l] ^ nums[l + 1]).bit_length() - 1
+        if v >= 0:
+            if nums[l] >> v & 1: 
+                c1[v] -= 1
+            else:
+                c0[v] -= 1
+    l += 1
+
+print(ans)
