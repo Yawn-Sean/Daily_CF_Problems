fae4a2905b6431b9340d0294a3c142be1fd65b94
Cyril Huang <hccyril@163.com>
2025-12-22 20:40:49 +0800

    20251222_liryc_mod

--- daily_problems/2025/12/1222/personal_submission/cf105790k_liryc.py
+++ daily_problems/2025/12/1222/personal_submission/cf105790k_liryc.py
@@ -1,6 +1,32 @@
 '''
-20251222_Y1
+https://codeforces.com/gym/105790/submission/354557776
 '''
+MOD = 998244353
+
+def multiply(a, b):
+    global MOD
+    return [
+        [(a[0][0]*b[0][0] + a[0][1]*b[1][0]) % MOD,
+            (a[0][0]*b[0][1] + a[0][1]*b[1][1]) % MOD],
+        [(a[1][0]*b[0][0] + a[1][1]*b[1][0]) % MOD,
+            (a[1][0]*b[0][1] + a[1][1]*b[1][1]) % MOD]
+    ]
+
+def matrix_pow(mat, power):
+    global MOD
+    result = [[1, 0], [0, 1]]  
+    while power:
+        if power & 1:
+            result = multiply(result, mat)
+        mat = multiply(mat, mat)
+        power >>= 1
+    return result
+
 def solve(n: int) -> int:  
-    MOD = 998244353
-    return pow(2, matrix_pow([[1, 1], [1, 0]], n, MOD - 1)[0][1], MOD)
+    global MOD
+    if n == 0:
+        return 1
+    MOD -= 1
+    e = matrix_pow([[1, 1], [1, 0]], n - 1)[0][0]
+    MOD += 1
+    return pow(2, e, MOD)
