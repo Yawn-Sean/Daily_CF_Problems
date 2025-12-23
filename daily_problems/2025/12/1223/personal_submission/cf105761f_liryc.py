495620f35de85f97bbb2600e8ce52d6ff84e8d86
hccyril <12328307+hccyril@users.noreply.github.com>
2025-12-23 12:21:45 +0800

    20251223_liryc

--- daily_problems/2025/12/1223/personal_submission/cf105761f_liryc.py
+++ daily_problems/2025/12/1223/personal_submission/cf105761f_liryc.py
@@ -1,27 +1,15 @@
 '''
-https://codeforces.com/gym/105761/submission/354580361
+https://codeforces.com/gym/105761/submission/354636728
 '''
-# binary search (WA)
 def solve(n: int, p: int) -> int:
-    def getp(x):
-        nonlocal n, p
-        if x <= 1:
-            return 99999 # > maximum(p)
-        elif x == n:
-            return 1
-        m, r = n, 1
-        while m > x:
-            r += 1
-            m = (m + x - 1) // x
-        return r
-    def getw(x):
-        nonlocal n, p
-        if x == n:
-            return n - 1
-        m, w = n, 0
-        while m > x:
-            w += x - 1
-            m = (m + x - 1) // x
-        return w + m - 1
-    min_x = bisect_left(range(n + 1), True, key=lambda x: getp(x) <= p)
-    return getw(min_x)
+    if p == 1:
+        return n - 1
+    elif p > 20: 
+        p = 20
+    dp = [[1] * (n + 1) for _ in range(p + 1)]    
+    for i in range(1, p + 1):
+        for j in range(1, n + 1):
+            dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1]
+            if dp[i][j] > n:
+                dp[i][j] = n
+    return bisect_left(dp[p], n)
