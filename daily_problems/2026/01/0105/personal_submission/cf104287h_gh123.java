0ab253db9bb31a249faeac0d14a4a2178c15a7a8
Yawn-Sean <106463906+Yawn-Sean@users.noreply.github.com>
2026-01-05 19:13:25 +0800

    Rename class from F104287 to H104287

--- daily_problems/2026/01/0105/personal_submission/cf104287h_gh123.java
+++ daily_problems/2026/01/0105/personal_submission/cf104287h_gh123.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class F104287 {
+public class H104287 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
