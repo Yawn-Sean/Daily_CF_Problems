8dd570752ac069a5e1fe3d87ed84c59f8c815d25
a-cute-province <2367559181@qq.com>
2026-01-05 10:43:57 +0800

    Create cf104287f_a-cute-province.cpp

---
+++ daily_problems/2026/01/0105/personal_submission/cf104287f_a-cute-province.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define x first
+#define y second
+#define cute_prov0nce main
+#define int long long
+using namespace std;
+#define all(x) x.begin(),x.end()
+typedef pair<int, int> PII;
+using i64 = long long;
+const int mod =  998244353;
+const int N = 1e6 + 10;
+
+void solve()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << c - std::gcd(c, a*b/__gcd(a, b)) << '\n';
+}
+signed cute_prov0nce()
+{
+#ifdef ACM_LOCAL
+    freopen("data.in", "r", stdin);
+    freopen("data.out", "w", stdout);
+#endif
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int t;
+    //t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
