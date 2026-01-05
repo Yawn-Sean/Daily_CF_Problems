e0ad098f2519b252c73b7bbe9823cbee710f6015
gxylengli <96274730+gxylengli@users.noreply.github.com>
2026-01-05 16:07:06 +0800

    20260105
    20260105

---
+++ daily_problems/2026/01/0105/personal_submission/cf104287f_lengli.cpp
@@ -0,0 +1,27 @@
+/*
+lengli_QAQ
+Hope there are no bugs!!!
+*/
+#include <bits/stdc++.h>
+#define fastio std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0)
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define i64 long long
+
+void solve(){
+    i64 a,b,c;
+    std::cin>>a>>b>>c;
+    i64 g=std::lcm(a,b);
+    i64 res=std::gcd(c,g);
+    std::cout<<c-res<<"\n";
+}
+
+signed main(){
+    fastio;
+    
+    int T;
+    std::cin>>T;
+    while(T--) solve();
+    
+    return 0;
+}
