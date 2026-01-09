0fd049cfc24788a57f26c69509ae6304ad58e68e
Cyril Huang <hccyril@163.com>
2026-01-09 23:56:53 +0800

    20260110_liryc

--- daily_problems/2026/01/0110/personal_submission/cf106296c_liryc.py
+++ daily_problems/2026/01/0110/personal_submission/cf106296c_liryc.py
@@ -1,22 +1,16 @@
 '''
-Docstring for daily_problems.2026.01.0110.personal_submission.cf106296c_liryc
+https://codeforces.com/gym/106296/submission/357107027
 '''
-# ref, binary search 
-from bisect import bisect_left
-
 def solve(n: int) -> list[int]:
     def ask(a, b):
         print(a, b, flush=True)
         return input()
 
-    def check(m):
-        return m and ask(2, (m << 1) - 1) != ask(1, m << 1)
-    
-    f = bisect_left(range(n + 3 >> 1), True, key=check)
-    return [3, (f << 1) - 1, f << 1 | 1]
-
-if __name__ == '__main__':
-    for _ in range(int(input())):
-        n = int(input())
-        ans = solve(n)
-        print(*ans, flush=True)
+    l, r = 1, n + 1 >> 1
+    while l <= r:
+        m = l + r >> 1
+        if ask(2, (m << 1) - 1) == ask(1, m << 1): 
+            l = m + 1
+        else: 
+            r = m - 1
+    return [3, (l << 1) - 3, (l << 1) - 1]
