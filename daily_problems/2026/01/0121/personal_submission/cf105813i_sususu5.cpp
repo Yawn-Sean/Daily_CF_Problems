5bc6aa192f11aa130ce7debee8929f3e5ebafea6
David Hua <tkrd1205@gmail.com>
2026-01-21 18:42:28 +1100

    feat(cf105813i): added cf105813i_sususu5.cpp

---
+++ daily_problems/2026/01/0121/personal_submission/cf105813i_sususu5.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <bit>
+#include <climits>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <ranges>
+#include <stack>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <numbers>
+#include <cmath>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+
+const int MOD = 1e9 + 7;
+
+ll query(int l, int r) {
+    cout << "? " << l << ' ' << r << '\n';
+    cout.flush();
+    ll x; cin >> x;
+    if (x == -1) exit(0);
+    return x;
+}
+
+void report(ll m) {
+    cout << "! " << m << '\n';
+    cout.flush();
+}
+
+void solve() {
+    int a, b, n;
+    cin >> a >> b >> n;
+    if (n > a + 2ll * (2 * b - 1)) cout << "Bob\n";
+    else if (n <= a + 2ll * (b - 1)) cout << "Alice\n";
+    else if (n < 3ll * a) cout << "Bob\n";
+    else cout << "Alice\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
