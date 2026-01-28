ff293445269ca0474b74f8f3a868ff86f6ec4d5b
Meguhine <131859484+EricMegalovania@users.noreply.github.com>
2026-01-28 14:57:12 +0800

    20260128 cf105198m_Meguhine.cpp

---
+++ daily_problems/2026/01/0128/personal_submission/cf105198m_Meguhine.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include<local/dbg.h>
+#else
+#define dbg(...) 42
+#endif
+
+using LL=long long;
+#define ai(x) using ai##x=array<int,x>;
+ai(2);ai(3);ai(4);ai(5);ai(6);ai(7);ai(8);ai(9);ai(10);ai(26);
+#undef ai
+
+#define all(x) (x).begin(),(x).end()
+#define allr(x) (x).rbegin(),(x).rend()
+template<class T>inline bool updmx(T& x,const T& y){return x<y?x=y,1:0;}
+template<class T>inline bool updmn(T& x,const T& y){return y<x?x=y,1:0;}
+template<class T>inline void clr(T& x){T().swap(x);}
+
+mt19937_64 rng(chrono::system_clock::now().time_since_epoch().count());
+LL rand(LL l,LL r){return rng()%(r-l+1)+l;}
+
+void solve(){
+	LL sx,sy,tx,ty,k;
+	cin>>sx>>sy>>tx>>ty>>k;
+	LL dx=abs(tx-sx),dy=abs(ty-sy);
+	if(dx<dy) swap(dx,dy);
+	// now dx>=dy
+	LL ans=dx+dy;
+	const LL d=dx-(dy+1)*k;
+	if(d>0){
+		LL c=(d-1)/k+1;
+		if(c&1) ++c;
+		ans+=c;
+	}
+	cout<<ans<<'\n';
+}
+int main(){
+#ifndef LOCAL
+	cin.tie(nullptr)->sync_with_stdio(false);
+#endif
+	int t; cin>>t;
+	while(t--) solve();
+	return 0;
+}
