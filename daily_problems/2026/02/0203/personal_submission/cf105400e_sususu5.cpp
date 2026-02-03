8d6eeb40457d21ac00377087899a13e31e593365
David Hua <tkrd1205@gmail.com>
2026-02-03 16:58:34 +1100

    feat(cf105400e): added cf105400e_sususu5.cpp

---
+++ daily_problems/2026/02/0203/personal_submission/cf105400e_sususu5.cpp
@@ -0,0 +1,72 @@
+#include <algorithm>
+#include <bit>
+#include <climits>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <ranges>
+#include <stack>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <numbers>
+#include <cmath>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using tiii = tuple<int, int, int>;
+
+const int MOD = 1e9 + 7;
+
+ll query(int l, int r) {
+    cout << "? " << l << ' ' << r << '\n';
+    cout.flush();
+    ll x; cin >> x;
+    if (x == -1) exit(0);
+    return x;
+}
+
+void report(ll m) {
+    cout << "! " << m << '\n';
+    cout.flush();
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    int mn = 11, mx = 0;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        mn = min(mn, a[i]);
+        mx = max(mx, a[i]);
+    }
+    int ans = 1;
+    if (mn == mx) {
+        for (int i = 0; i < n; i++) ans *= 10;
+    } else {
+        for (int i = 0; i < n; i++) {
+            if (a[i] == mn) ans *= a[i];
+            if (a[i] == mx) ans *= (11 - a[i]);
+        }
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
