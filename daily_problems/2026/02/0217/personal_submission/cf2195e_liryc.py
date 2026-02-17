22795d3b58d276fc88619580a5837523da8b668b
Cyril Huang <hccyril@163.com>
2026-02-17 21:05:21 +0800

    20260217_liryc_t1

--- daily_problems/2026/02/0217/personal_submission/cf2195e_liryc.py
+++ daily_problems/2026/02/0217/personal_submission/cf2195e_liryc.py
@@ -1,51 +1,20 @@
 '''
-2026/2/17 Y1
-1500
+https://codeforces.com/contest/2195/submission/363398698
 '''
 # tree dp
-from sys import stdin
-input = lambda: stdin.readline().strip()
-pmax = lambda x, y: y if y > x else x
-pmin = lambda x, y: y if y < x else x
-# pmin = lambda x, y: x if y < 0 or x >= 0 and x <= y else y
-
-def init():
-    n = int(input())
-    tg = [[] for _ in range(n + 1)]
-    tg[0].append(1)
-    for i in range(1, n + 1):
-        l, r = map(int, input().split())
-        if l == r == 0:
-            pass
-        else:
-            tg[i][:] = l, r
-    return n, tg
-
 def solve(n: int, tg: list[list[int]]):
     MOD = 1000000007
-    qu = [0]
-    pr = [-1] * (n + 1)
-    for u in qu:
+    po = [1]
+    for u in po:
         for v in tg[u]:
-            pr[v] = u
-            qu.append(v)
-
-    ans = [0] * (n + 1)
-    
-    for i in reversed(qu):
-        if tg[i] and i > 0:
-            u, v = tg[i]
-            ans[i] = (ans[u] + 2 + ans[v] + 2) % MOD
-    
-    for i in qu:
-        for j in tg[i]:
-            ans[j] += ans[i] + 1
-            ans[j] %= MOD
-    
-    return ans[1:]
-
-if __name__ == '__main__':
-    for _ in range(int(input())):
-        args = init()
-        ans = solve(*args)
-        print(*ans)
+            po.append(v)
+    b = [0] * (n + 1)
+    for u in po[::-1]:
+        for v in tg[u]:
+            b[u] = (b[u] + b[v] + 2) % MOD
+    a = [0] * (n + 1)
+    a[1] = (b[1] + 1) % MOD
+    for u in po:
+        for v in tg[u]:
+            a[v] = (b[v] + a[u] + 1) % MOD
+    return a[1:]
