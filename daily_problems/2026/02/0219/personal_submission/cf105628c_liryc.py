fab9a15919605823c01c22514dcbce1689debb25
Cyril Huang <hccyril@163.com>
2026-02-19 23:18:04 +0800

    20260219_liryc_bugfix

--- daily_problems/2026/02/0219/personal_submission/cf105628c_liryc.py
+++ daily_problems/2026/02/0219/personal_submission/cf105628c_liryc.py
@@ -1,30 +1,17 @@
 '''
-2026/2/19 Y1
-1000
+https://codeforces.com/gym/105628/submission/363621766
 '''
 # gcd and lcm
-from sys import stdin
-input = lambda: stdin.readline().strip()
-from math import lcm
-
-def init():
-    n, l, r, k = map(int, input().split())
-    a = list(map(int, input().split()))
-    return n, l, r, k, a
-
 def solve(n: int, l: int, r: int, k: int, a: list[int]):
-    if r < k or min(a) < k:
+    min_a = min(a)
+    if min_a <= k:
         return -1
+    if l <= k <= r and min_a > k:
+        return k
     m = 1
     for e in a:
         m = lcm(m, e)
         if m > r:
             return -1
-    # m = reduce(lcm, a) # m will be extremely large and cause TLE
     x = (r - k) // m * m + k
     return x if l <= x <= r else -1
-
-if __name__ == '__main__':
-    args = init()
-    ans = solve(*args)
-    print(ans)
