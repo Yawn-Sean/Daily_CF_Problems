b1a7ce3e413aedcde04eb3980cf2f28430411786
Cyril Huang <hccyril@163.com>
2026-02-21 15:56:00 +0800

    20260221_liryc_t1

--- daily_problems/2026/02/0221/personal_submission/cf104855d_liryc.py
+++ daily_problems/2026/02/0221/personal_submission/cf104855d_liryc.py
@@ -1,26 +1,21 @@
 '''
-https://codeforces.com/gym/104855/submission/363760691
+https://codeforces.com/gym/104855/submission/363785580
 '''
 # graph
-def solve(n: int, m: int, edges: list[list[int]]):
-    ug = [[] for _ in range(n)]
-    for u, v, c in edges:
-        u, v = u - 1, v - 1
-        ug[u].append([v, c])
+def solve(n: int, m: int, tg: list[list[list[int]]]):
     vis = [0] * n
     vis[0] = -1
-    ans = [1]
     qu = deque()
     qu.append(0)
     while qu:
         u = qu.popleft()
-        for v, c in ug[u]:
+        for v, c in tg[u]:
             if c != vis[u] and vis[v] >= 0 and c != vis[v]:
                 if vis[v] == 0:
                     vis[v] = c
-                    ans.append(v + 1)
                 else:
                     vis[v] = -1
                 qu.append(v)
-    ans.sort()
-    return ans
+    for i in range(n):
+        if vis[i]:
+            yield i + 1
