98caa2440669bf077bcde6ac7bd7add73f14eecb
hccyril <12328307+hccyril@users.noreply.github.com>
2026-03-02 19:41:35 +0800

    20260302_liryc_fixTLE

--- daily_problems/2026/03/0302/personal_submission/cf106390b_liryc.py
+++ daily_problems/2026/03/0302/personal_submission/cf106390b_liryc.py
@@ -1,26 +1,25 @@
 '''
-https://codeforces.com/gym/106390/submission/365076062
+https://codeforces.com/gym/106390/submission/365078953
 '''
 
-# tg: tree graph with node 0-indexed
+# tg: tree graph with node 0-indexed, encoded = (w << 20 | u)
 def solve(n: int, tg: list[list[int]]) -> list[int]:
     pr = [-1] * n
     pr[0] = 0
     d = [0] * n
-    md = [0] 
+    md = [-1] * n 
+    md[0] = 0
     lv = [0] * n
     qu = [0]
     for u in qu:
-        for v, w in tg[u]:
+        for e in tg[u]:
+            v, w = e & (1 << 20) - 1, e >> 20 
             if v != pr[u]:
                 qu.append(v)
                 level = lv[v] = lv[u] + 1
                 pr[v] = u
                 d[v] = d[u] + w
-                if level >= len(md):
-                    md.append(d[v])
-                elif d[v] < md[level]:
-                    md[level] = d[v]
+                md[level] = pmin(md[level], d[v])
     ans = [0] * n
     for u in qu:
         if u:
