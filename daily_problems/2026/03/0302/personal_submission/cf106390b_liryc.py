86364fedbee619f8fe5650bc2141b473bd6d0d86
hccyril <12328307+hccyril@users.noreply.github.com>
2026-03-02 19:18:34 +0800

    20260302_liryc_t1

--- daily_problems/2026/03/0302/personal_submission/cf106390b_liryc.py
+++ daily_problems/2026/03/0302/personal_submission/cf106390b_liryc.py
@@ -1,28 +1,28 @@
 '''
-2026.3.2 Y1 1600
-tree graph
+https://codeforces.com/gym/106390/submission/365076062
 '''
-def solve(n, tg):     
-    INF = 10 ** 18
-    tg = [[] for _ in range(n)]
-    parent = [-1] * n
-    dis0 = [0] * n
-    dis1 = [0] * n
-    min_dis = [INF] * n
-    que = [0]
-    for u in que:
-        min_dis[dis0[u]] = min(min_dis[dis0[u]], dis1[u])
-        for msk in tg[u]:
-            w, v = divmod(msk, n)
-            if parent[u] != v:
-                parent[v] = u
-                dis0[v] = dis0[u] + 1
-                dis1[v] = dis1[u] + w
-                que.append(v)
-    for u in que:
-        dis1[u] = min(dis1[u], min_dis[dis0[u]])
-        for msk in tg[u]:
-            w, v = divmod(msk, n)
-            if parent[v] == u:
-                dis1[v] = dis1[u] + w
-    return dis1    
+
+# tg: tree graph with node 0-indexed
+def solve(n: int, tg: list[list[int]]) -> list[int]:
+    pr = [-1] * n
+    pr[0] = 0
+    d = [0] * n
+    md = [0] 
+    lv = [0] * n
+    qu = [0]
+    for u in qu:
+        for v, w in tg[u]:
+            if v != pr[u]:
+                qu.append(v)
+                level = lv[v] = lv[u] + 1
+                pr[v] = u
+                d[v] = d[u] + w
+                if level >= len(md):
+                    md.append(d[v])
+                elif d[v] < md[level]:
+                    md[level] = d[v]
+    ans = [0] * n
+    for u in qu:
+        if u:
+            ans[u] = pmin(md[lv[u]], ans[pr[u]] + d[u] - d[pr[u]])
+    return ans
