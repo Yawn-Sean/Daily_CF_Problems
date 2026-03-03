e44a7a297c10f74c713a2788bd396a4583242515
hccyril <12328307+hccyril@users.noreply.github.com>
2026-03-03 16:37:18 +0800

    20260303_liryc_t1

--- daily_problems/2026/03/0303/personal_submission/cf104669l_liryc.py
+++ daily_problems/2026/03/0303/personal_submission/cf104669l_liryc.py
@@ -1,22 +1,26 @@
 '''
-2026.3.3 Y1 1700
+https://codeforces.com/gym/104669/submission/365190720
 '''
-def solve(a: int, b: int) -> int: 
-    sm = (2 * a + b - 1) * b // 2
+# math
+def solve(a: int, b: int) -> int:
+    sm = a * b + (b - 1) * b // 2
+    if b & 3 == 0:
+        return sm >> 1
     def check(x):
-        for i in range(1, b):
-            l = a * i + i * (i - 1) // 2
-            r = a * i + (2 * b - 1 - i) * i // 2
-            if r // x * x >= l: 
+        nonlocal sm, a, b
+        for c in range(1, b):
+            l = a * c + (c - 1) * c // 2
+            r = a * c + (b - 1 + b - c) * c // 2
+            if r - r % x >= l:
                 return True
         return False
-    ans = 0
-    for i in range(1, 10 ** 5 * 2):
-        if i * i > sm: 
+    ans = 1
+    for x in range(2, sm):
+        if x * x > sm:
             break
-        if sm % i == 0:
-            if check(i): 
-                ans = max(ans, i)
-            if check(sm // i): 
-                ans = max(ans, sm // i)
+        if sm % x == 0:
+            if check(sm // x):
+                return sm // x
+            elif x > ans and check(x):
+                ans = x
     return ans
