7f0b054c1b8d891e6d6b30b594cf63eab355d1b7
Cyril Huang <hccyril@163.com>
2026-03-05 22:12:15 +0800

    20260305_liryc_t1

--- daily_problems/2026/03/0305/personal_submission/cf106404h_liryc.py
+++ daily_problems/2026/03/0305/personal_submission/cf106404h_liryc.py
@@ -1,30 +1,23 @@
 '''
-20260305 Y1 1700
+https://codeforces.com/gym/106404/submission/365480791
 '''
+# dijkstra
 def solve(n: int, m: int, a: list[int], tg: list[list[int]]) -> bool: 
-    INF = (1 << 30) - 1 << 1
-    tg = [[] for _ in range(n)]
-    pq = [a[i] * n + i for i in range(n)]
-    while pq:
-        e = heappop(pq)
-        d, u = e >> 18, e & (1 << 18) - 1
-        if a[u] == d:
-            for e in tg[u]:
-                nd, v = e >> 18, e & (1 << 18) - 1
-                if nd + d < a[v]:
-                    a[v] = nd + d
-                    heappush(pq, a[v] << 18 | v)
-    dis = [INF] * n
-    dis[0] = 0
-    pq = [0]
-    while pq:
-        e = heappop(pq)
-        d, u = e >> 18, e & (1 << 18) - 1
-        if dis[u] == d:
-            for e in tg[u]:
-                nd, v = e >> 18, e & (1 << 18) - 1
-                if nd + d < dis[v] and nd + d <= a[v]:
-                    dis[v] = nd + d
-                    heappush(pq, dis[v] << 18 | v)
-    return dis[n - 1] < INF
-
+    enc = lambda w, x: w << 18 | x
+    dec = lambda e: (e >> 18, e & (1 << 18) - 1)
+    da = [-1] * n
+    da[0] = 0
+    hp = [0]
+    while hp:
+        s, u = dec(heappop(hp))
+        if da[u] != s:
+            continue
+        for e in tg[u]:
+            w, v = dec(e)
+            t = pmin(da[v], s + w)
+            if t != da[v] and t <= a[v]:
+                if v == n - 1:
+                    return True
+                da[v] = t
+                heappush(hp, enc(t, v))
+    return False
