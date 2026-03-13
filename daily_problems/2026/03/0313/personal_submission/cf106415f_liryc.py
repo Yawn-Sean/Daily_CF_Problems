816fe9333f26d5df67404538af770ca515e1f8ae
hccyril <12328307+hccyril@users.noreply.github.com>
2026-03-13 20:09:57 +0800

    20260313_liryc_t1

--- daily_problems/2026/03/0313/personal_submission/cf106415f_liryc.py
+++ daily_problems/2026/03/0313/personal_submission/cf106415f_liryc.py
@@ -1,9 +1,37 @@
 '''
-20260313 Y1 1400
+https://codeforces.com/gym/106415/submission/366491506
 '''
-# ref
-def solve(x):
-    if x & -x == x:
+# interactive problem
+
+from functools import cache
+import sys
+input = lambda: sys.stdin.readline().strip()
+
+def init():
+    n = int(input())
+    sa = list(map(int, input().split()))
+    return n, sa
+
+def query(n, m):
+    print('?', n, m, flush=True)
+    return int(input()[2:])
+
+def answer(x):
+    print('!', x, flush=True)
+    return input()
+
+@cache
+def findf(x):
+    if not x & x - 1:
         return query(x, x) // 2
-    v = 1 << x.bit_length()
-    return query(v - x, x) - solve(v - x)
+    y = (1 << x.bit_length()) - x
+    return query(y, x) - findf(y)
+
+def solve(n: int, sa: list[int]) -> list[int]:
+    return [findf(x) for x in sa]
+
+if __name__ == '__main__':
+    args = init()
+    ans = solve(*args)
+    for x in ans:
+        answer(x)
