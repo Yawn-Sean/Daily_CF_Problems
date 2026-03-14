07bfdbb53e954be4d6f97415e666ba85cef9c817
guanghuichen1 <48914833+guanghuichen1@users.noreply.github.com>
2026-03-14 19:48:09 +0800

    20260314 Add cf102569j class with main method output

---
+++ daily_problems/2026/03/0314/personal_submission/cf102569j_gh123.java
@@ -0,0 +1,8 @@
+public class cf102569j {
+    public static void main(String[] args) {
+        System.out.println("3");
+        System.out.println("3 3 4");
+        System.out.println("3");
+        System.out.println("1 1 7");
+    }
+}
