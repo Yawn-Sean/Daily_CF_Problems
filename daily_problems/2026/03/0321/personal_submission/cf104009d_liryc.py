eaf5790184d4fd55315e594c6db2fa96541e41f5
Cyril Huang <hccyril@163.com>
2026-03-21 20:18:40 +0800

    20260321_liryc_t1

--- daily_problems/2026/03/0321/personal_submission/cf104009d_liryc.py
+++ daily_problems/2026/03/0321/personal_submission/cf104009d_liryc.py
@@ -1,19 +1,13 @@
 '''
-20260321 Y1 1600
+https://codeforces.com/gym/104009/submission/367593232
 '''
-def solve(n: int, m: int, edges: list[list[int]]) -> int: 
-    uni = UnionFind(n * 2)
+def solve(n: int, m: int, edges: list[list[int]]) -> int:
+    uni = UnionFind(n << 1)
     ans = 1
-    cur = []
     for u, v in edges:
         if uni.check(u, v):
-            for x in cur:
-                uni.back(x)
-                uni.back(x + n)
+            uni.reset()
             ans += 1
-            cur.clear()
         uni.union(u, v + n)
-        uni.union(v, u + n)
-        cur.append(u)
-        cur.append(v)
+        uni.union(v, u + n)    
     return ans
