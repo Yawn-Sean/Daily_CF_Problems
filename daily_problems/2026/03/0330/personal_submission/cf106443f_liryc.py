3b5984e131a569eb795cfd3689fc758453c6faaa
Cyril Huang <hccyril@163.com>
2026-03-30 08:22:12 +0800

    20260330_liryc_bugfix

--- daily_problems/2026/03/0330/personal_submission/cf106443f_liryc.py
+++ daily_problems/2026/03/0330/personal_submission/cf106443f_liryc.py
@@ -1,10 +1,10 @@
 '''
-https://codeforces.com/gym/106443/submission/368716443
+https://codeforces.com/gym/106443/submission/368793712
 '''
 # probability
 def solve(n: int) -> int:
     MOD = 1000000007
     b = (n - 1 >> 1) * (n - 2) % MOD if n & 1 else (n - 1) * (n - 2 >> 1) % MOD
-    m = (n + 1 >> 1) - 1
+    m = (n - 1 >> 1) + (n & 1) - 1 
     a = ((1 + m) * m >> 1) % MOD
     return a * pow(b, -1, MOD) % MOD
