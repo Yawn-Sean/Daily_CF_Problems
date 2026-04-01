ccc8ea57a75496299312164d469dfa062097e38d
hccyril <12328307+hccyril@users.noreply.github.com>
2026-04-01 19:16:53 +0800

    20260401_liryc_t1

--- daily_problems/2026/04/0401/personal_submission/cf106443m_liryc.py
+++ daily_problems/2026/04/0401/personal_submission/cf106443m_liryc.py
@@ -1,28 +1,36 @@
 '''
-20260401 Y1 1400
+https://codeforces.com/gym/106443/submission/369098175
 '''
-def solve(n: int, m: int, a: list[int]) -> int: 
-    dk = [0] * (m * 2 + 1)
-    db = [0] * (m * 2 + 1)
-    v = (m + 1) // 2
-    for x in a:
-        dk[x] += 1
-        dk[x + v] -= 1
-        db[x] -= x
-        db[x + v] += x
-        
-        dk[x + v] -= 1
-        dk[x + m] += 1
-        db[x + v] += x + m
-        db[x + m] -= x + m
-    
-    for i in range(m * 2):
-        dk[i + 1] += dk[i]
-        db[i + 1] += db[i]
-    
+def solve(n: int, m: int, a: list[int]) -> int:
+    if m == 2:
+        return min(a.count(0), a.count(1))
+    a.sort(reverse=True)
+    if a[0] == a[-1]: 
+        return 0 
+    lq, rq = deque(), deque()
+    lc, rc, d = 0, 0, 0
     ans = [0] * m
-    
-    for i in range(m * 2):
-        ans[i % m] += dk[i] * i + db[i]
-    
+    for x in a:
+        if x == 0:
+            lq.append(x)
+            lc += 1
+        elif x < m - x:
+            rq.appendleft(x)
+            rc += 1
+            d += x
+        else:
+            lq.appendleft(x)
+            lc += 1
+            d += m - x
+    ans[0] = d
+    for i in range(1, m):
+        d += lc - rc
+        while rq and rq[0] == i:
+            lq.append(rq.popleft())
+            lc, rc = lc + 1, rc - 1
+        while lq and (i - lq[0]) % m > (lq[0] - i) % m:
+            lc, rc = lc - 1, rc + 1
+            d += (lq[0] - i) % m - (i - lq[0]) % m
+            rq.append(lq.popleft())
+        ans[i] = d
     return min(ans)
