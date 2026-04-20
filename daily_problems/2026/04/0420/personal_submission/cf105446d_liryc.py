4e9d7772bb5fed22632f8991b0c3961fa9ab60cf
hccyril <12328307+hccyril@users.noreply.github.com>
2026-04-20 10:02:25 +0800

    20260420_liryc_t1

--- daily_problems/2026/04/0420/personal_submission/cf105446d_liryc.py
+++ daily_problems/2026/04/0420/personal_submission/cf105446d_liryc.py
@@ -4,7 +4,7 @@ https://codeforces.com/gym/105446/submission/371824611
 def solve(p: float, r: float, y: float) -> list[float]:
     check = lambda x: max(abs(x), abs(x - r), abs((p + y + r) / 2 - x), abs((y + r - p) / 2 - x))
     lo, hi = -2.0, 2.0
-    ans = (lo + hi) / 2
+    a = (lo + hi) / 2
     for _ in range(128):
         m1 = (2 * lo + hi) / 3
         m2 = (lo + 2 * hi) / 3
@@ -12,9 +12,9 @@ def solve(p: float, r: float, y: float) -> list[float]:
             hi = m2
         else:
             lo = m1
-        ans1 = (lo + hi) / 2
-        diff = abs(ans - ans1)
-        ans = ans1
+        b = (lo + hi) / 2
+        diff = abs(a - b)
+        a = b
         if diff < 1e-6:
             break
-    return [ans, (p + y + r) / 2 - ans, ans - r, (y + r - p) / 2 - ans]
+    return [a, (p + y + r) / 2 - a, a - r, (y + r - p) / 2 - a]
