44d149f37b45a02da5e5e53e40607940118ac884
Yawn-Sean <1900015431@pku.edu.cn>
2026-04-20 14:42:49 +0800

    20260419 Yawn Sean's submission for CF105446D

--- daily_problems/2026/04/0420/personal_submission/cf105446d_yawn_sean.py
+++ daily_problems/2026/04/0420/personal_submission/cf105446d_yawn_sean.py
@@ -1,28 +1,24 @@
 # Submission link: https://codeforces.com/gym/105446/submission/371753618
 def main():
-    n, k = MII()
-    mod = 10 ** 9 + 7
+    t = II()
+    outs = []
     
-    ans = 0
-    dp = [0] * (n + 1)
-    dp[0] = 1
+    for _ in range(t):
+        p, r, y = LFI()
+        left, right = -2, 2
+        
+        def f(x):
+            return max(abs(x), abs(x - r), abs((p + y + r) / 2 - x), abs((y + r - p) / 2 - x))
+        
+        for _ in range(100):
+            mid1 = (left * 2 + right) / 3
+            mid2 = (left + right * 2) / 3
+            
+            if f(mid1) < f(mid2): right = mid2
+            else: left = mid1
     
-    for i in range(k):
-        ndp = [0] * (n + 1)
-        
-        for j in range(n + 1):
-            if i and i + j <= n:
-                dp[i + j] += dp[j]
-                dp[i + j] %= mod
-            if i + 1 <= n and j + 1 <= n:
-                ndp[j + 1] += dp[j]
-                ndp[j + 1] %= mod
-        
-        y = k - i
-        for x in range(1, n // y + 1):
-            ans += x * x * dp[n - y * x] % mod
-            ans %= mod
-        
-        dp = ndp
+        x = (left + right) / 2
+        x = round(x, 10)
+        outs.append(f'{x} {(p + y + r) / 2 - x} {x - r} {(y + r - p) / 2 - x}')
     
-    print(ans)
+    print('\n'.join(outs))
