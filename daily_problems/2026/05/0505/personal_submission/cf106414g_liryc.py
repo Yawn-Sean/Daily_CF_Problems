cae2da1d1c2a04af8ecc7c3f17b50167a97b9e27
Cyril Huang <hccyril@163.com>
2026-05-05 17:18:35 +0800

    20260505_liryc_bugfix

--- daily_problems/2026/05/0505/personal_submission/cf106414g_liryc.py
+++ daily_problems/2026/05/0505/personal_submission/cf106414g_liryc.py
@@ -1,19 +1,7 @@
 '''
-https://codeforces.com/gym/106414/submission/373563708
-2026/5/5 Y1
-1400
+https://codeforces.com/gym/106414/submission/373578574
 prefix + suffix, 2nd max
 '''
-
-from sys import stdin
-from collections import Counter
-input = lambda: stdin.readline().strip()
-
-def init():
-    n = int(input())
-    a = list(map(int, input().split()))
-    return n, a
-
 def solve(n: int, a: list[int]) -> int:
     if all(x == a[0] for x in a):
         return 0
@@ -43,13 +31,10 @@ def solve(n: int, a: list[int]) -> int:
         elif m > m2:
             m2 = m        
         if t == st[i + 1]:
-            ans = max(ans, m1 + sm2[i + 1], m2 + sm1[i + 1])
+            if sm2[i + 1] > 0:
+                ans = max(ans, m1 + sm2[i + 1])
+            if m2 > 0:
+                ans = max(ans, m2 + sm1[i + 1])
         else:
             ans = max(ans, m1 + sm1[i + 1])
     return ans
-
-if __name__ == '__main__':
-    for _ in range(int(input())):
-        args = init()
-        ans = solve(*args)
-        print(ans)
