e9a900b1d825b495516abd9fda6c6152fb841387
Yawn-Sean <1900015431@pku.edu.cn>
2026-05-12 08:27:33 +0800

    20260512

--- daily_problems/2026/05/0509/personal_submission/cf106414a_yawn_sean.cpp
+++ daily_problems/2026/05/0509/personal_submission/cf106414a_yawn_sean.cpp
@@ -1,38 +1,29 @@
 #include <bits/stdc++.h>
 // #pragma GCC optimize("O3,Ofast,unroll-loops")
 // #include "atcoder/all"
-#include "atcoder/segtree"
 
 using namespace std;
 auto rng = mt19937(random_device()());
 auto rngl = mt19937_64(random_device()());
-
-typedef pair<int, int> node;
-
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
 
-	int n;
-	cin >> n;
+	int t;
+	cin >> t;
 
-	vector<node> shirts(n);
-	for (auto &[x, y]: shirts) cin >> x >> y;
+	auto f = [&] (int x1, int x2) -> int {
+		int x = abs(x1 - x2);
+		if (!x) return 0;
+		return (x - 1) % 2 + 1;
+	};
 
-	sort(shirts.begin(), shirts.end(), [&] (node &x, node &y) {
-		if (x.second > y.second) return true;
-		if (x.second < y.second) return false;
-		return x.first < y.first;
-	});
-
-	int q;
-	cin >> q;
-
-	while (q --) {
-		int b;
-		cin >> b;
+	while (t --) {
+		int n, m, r1, c1, r2, c2;
+		cin >> n >> m >> r1 >> c1 >> r2 >> c2;
+		cout << f(r1, r2) + f(c1, c2) << '\n';
 	}
 
 	return 0;
