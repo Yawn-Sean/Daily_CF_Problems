66abc20bd51f055413ac3f6d8e026a0d95b6c712
Yawn-Sean <1900015431@pku.edu.cn>
2026-05-17 20:58:11 +0800

    20260517 Yawn Sean's submission for CF106141L

--- daily_problems/2026/05/0518/personal_submission/cf106141l_yawn_sean.cpp
+++ daily_problems/2026/05/0518/personal_submission/cf106141l_yawn_sean.cpp
@@ -11,6 +11,39 @@ int main() {
 	cin.tie(0);
 	cout.tie(0);
 
+	int n;
+	cin >> n;
+
+	vector<int> nums(n);
+	for (auto &x: nums) cin >> x;
+
+	vector<int> vals(n);
+	int total = 0, val = n;
+	for (int i = 0; i < n; i ++) {
+		vals[i] = (nums[i] >= 0 ? 1 : -1);
+		total += abs(nums[i]);
+	}
+
+	vector<int> order(n);
+	iota(order.begin(), order.end(), 0);
+	sort(order.begin(), order.end(), [&] (int i, int j) {return abs(nums[i]) > abs(nums[j]);});
+
+	int ma_total = total, ma_val = val, chosen = -1;
+
+	for (int i = 0; i < n; i ++) {
+		total += 2 * abs(nums[order[i]]);
+		val += 8;
+
+		if (1ll * total * total * ma_val > 1ll * ma_total * ma_total * val) {
+			ma_total = total;
+			ma_val = val;
+			chosen = i;
+		}
+	}
+
+	for (int i = 0; i <= chosen; i ++) vals[order[i]] *= 3;
+
+	for (auto &x: vals) cout << x << ' ';
 
 	return 0;
 }
