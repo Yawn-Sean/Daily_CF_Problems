71d7f900d71b354bdad29fd6beca4fea9b593eb7
hccyril <12328307+hccyril@users.noreply.github.com>
2026-05-19 17:20:05 +0800

    20260519_liryc_t1

--- daily_problems/2026/05/0519/personal_submission/cf106531e_liryc.py
+++ daily_problems/2026/05/0519/personal_submission/cf106531e_liryc.py
@@ -1,14 +1,20 @@
 '''
-20260519 1700
+https://codeforces.com/gym/106531/submission/375209933
+Math, simple geometry
 '''
 def solve(ax: int, ay: int, bx: int, by: int, m: int, lines: list[list[int]]) -> int:  
-    def dis(x1, y1, x2, y2):
-        return abs(x1 - x2) + abs(y1 - y2)
-    ans = dis(ax, ay, bx, by)
-    for b, k in lines:
-        for x1 in [ax, (ay - b) * k]:
-            y1 = k * x1 + b
-            for x2 in [bx, (by - b) * k]:
-                y2 = k * x2 + b
-                ans = min(ans, dis(ax, ay, x1, y1) + abs(x1 - x2) + dis(x2, y2, bx, by))
+    ans = abs(ax - bx) + abs(ay - by)
+    if ax == bx or ay == by:
+        return ans
+    ua, va = ay - ax, ay + ax
+    ub, vb = by - bx, by + bx
+    du = abs(ua - ub)
+    dv = abs(va - vb)
+    for k, d in lines:
+        if d == 1:
+            mu = abs(ua - k) + abs(ub - k)
+            ans = min(ans, max(mu, mu + dv >> 1))
+        else:
+            mv = abs(va - k) + abs(vb - k)
+            ans = min(ans, max(mv, mv + du >> 1))
     return ans
