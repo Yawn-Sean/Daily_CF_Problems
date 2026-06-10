db068e6a9220b14eb54c90e070c14ad9495f2aa4
hccyril <12328307+hccyril@users.noreply.github.com>
2026-06-10 19:05:19 +0800

    20260610_liryc_t1

--- daily_problems/2026/06/0610/personal_submission/cf104755f_liryc.py
+++ daily_problems/2026/06/0610/personal_submission/cf104755f_liryc.py
@@ -2,24 +2,19 @@
 https://codeforces.com/gym/104755/submission/378040502
 '''
 def solve(n: int, m: int, A: list[int], B: list[int]) -> list[int]:
-    bs = set(B)
     if m < n:
         return []
+    bs = set(B)
     la = []
     cp = set()
     a0 = A[0] 
     for b in B:
         c = 2 * a0 - b 
-        va = True
         for a in A:
             if (2 * a - c) not in bs:
-                va = False
                 break 
-        if va:
+        else:
             la.append(c)
             for a in A:
                 cp.add(2 * a - c)
-    if len(cp) == m:
-        return la
-    else:
-        return []
+    return la if len(cp) == m else []
