8bb56e2ffe533688ebbc36383686245227695125
Yawn-Sean <1900015431@pku.edu.cn>
2026-06-21 20:49:36 +0800

    20260621 Yawn Sean's submission for CF106592C

--- daily_problems/2026/06/0622/personal_submission/cf106592c_yawn_sean.cpp
+++ daily_problems/2026/06/0622/personal_submission/cf106592c_yawn_sean.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 // #pragma GCC optimize("O3,Ofast,unroll-loops")
 // #include "atcoder/all"
-#include "atcoder/dsu"
 
 using namespace std;
 auto rng = mt19937(random_device()());
@@ -16,90 +15,39 @@ int main() {
 	cin >> t;
 
 	while (t --) {
-		int n, m, xor_op;
-		cin >> n >> m >> xor_op;
+		long long k;
+		int n;
+		cin >> k >> n;
 
-		vector<array<int, 3>> edges(m);
-		for (auto &[u, v, w]: edges) {
-			cin >> u >> v >> w;
-			u --, v --;
+		vector<int> nums(n);
+		for (auto &x: nums) cin >> x;
+
+		int l = 0, r = 1e9;
+
+		while (l <= r) {
+			int mid = (l + r) / 2;
+			long long val = 0;
+
+			for (auto &x: nums) val += min(mid, x);
+
+			if (val > k) r = mid - 1;
+			else l = mid + 1;
 		}
 
-		sort(edges.begin(), edges.end(), [&] (array<int, 3> x, array<int, 3> y) {return x[2] < y[2];});
-
-		long long ans = 0;
-		atcoder::dsu uf(n);
-
-		vector<vector<pair<int, int>>> path(n);
-
-		for (auto &[u, v, w]: edges) {
-			if (uf.merge(u, v)) {
-				ans += w;
-				path[u].emplace_back(v, w);
-				path[v].emplace_back(u, w);
-			}
+		vector<int> ans(n);
+		for (int i = 0; i < n; i ++) {
+			ans[i] = min(nums[i], r);
+			k -= ans[i];
 		}
 
-		vector<vector<int>> nth_parent(18, vector<int>(n, -1));
-		vector<vector<int>> nth_edge(18, vector<int>(n, 0));
-		vector<int> depth(n, 0);
-
-		auto dfs = [&] (auto &self, int u, int p) -> void {
-			for (auto &[v, w]: path[u]) {
-				if (v != p) {
-					nth_parent[0][v] = u;
-					nth_edge[0][v] = w;
-					depth[v] = depth[u] + 1;
-					self(self, v, u);
-				}
-			}
-		};
-
-		dfs(dfs, 0, -1);
-
-		int diff = (m == n - 1 ? xor_op : 0);
-
-		for (int i = 0; i < 17; i ++) {
-			for (int u = 0; u < n; u ++) {
-				if (nth_parent[i][u] != -1) {
-					nth_parent[i + 1][u] = nth_parent[i][nth_parent[i][u]];
-					nth_edge[i + 1][u] = max(nth_edge[i][u], nth_edge[i][nth_parent[i][u]]);
-				}
+		for (int i = 0; i < n; i ++) {
+			if (ans[i] < nums[i] && k) {
+				ans[i] ++;
+				k --;
 			}
 		}
 
-		for (auto &[u, v, w]: edges) {
-			if (depth[u] > depth[v]) swap(u, v);
-
-			int cur = 0;
-			int d = depth[v] - depth[u];
-
-			while (d) {
-				int x = d & -d;
-				int bit = __builtin_ctz(x);
-				cur = max(cur, nth_edge[bit][v]);
-				v = nth_parent[bit][v];
-				d -= x;
-			}
-
-			if (u != v) {
-				for (int i = 17; i >= 0; i --) {
-					if (nth_parent[i][u] != nth_parent[i][v]) {
-						cur = max(cur, nth_edge[i][u]);
-						u = nth_parent[i][u];
-						cur = max(cur, nth_edge[i][v]);
-						v = nth_parent[i][v];
-					}
-				}
-
-				cur = max(cur, nth_edge[0][u]);
-				cur = max(cur, nth_edge[0][v]);
-			}
-
-			diff = min(diff, (w ^ xor_op) - cur);
-		}
-
-		cout << ans + diff;
+		for (int i = 0; i < n; i ++) cout << ans[i] << " \n"[i == n - 1];
 	}
 
 	return 0;
