eb9c60a513f60b830c568c316952aa1c8a222c32
hccyril <12328307+hccyril@users.noreply.github.com>
2026-06-25 17:56:09 +0800

    20260625_liryc

---
+++ daily_problems/2026/06/0625/personal_submission/cf106020h_liryc.py
@@ -0,0 +1,18 @@
+'''
+20260625 T1
+'''
+def solve(n: int, a: list[int]) -> int:
+    ans = 0
+    ia = [i for i in range(n) if a[i]]
+    
+    for i in range(1, len(ia)):
+        x = ia[i - 1]
+        y = ia[i]
+        if min(a[x], a[y]) == 1 and max(a[x], a[y]) <= 2:
+            lx = ia[i - 2] if i - 2 >= 0 else -1
+            ry = ia[i + 1] if i + 1 < len(ia) else n
+            
+            ans += (x - lx) * (ry - y)
+            if y - x == 1: ans -= 1
+    
+    return ans
