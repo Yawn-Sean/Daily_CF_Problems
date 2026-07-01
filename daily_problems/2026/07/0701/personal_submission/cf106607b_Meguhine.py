34f4391ffacb4b8958512e379ddad1075d0f0919
Meguhine <131859484+EricMegalovania@users.noreply.github.com>
2026-07-01 13:49:34 +0800

    20260701 cf106607b_Meguhine.py

---
+++ daily_problems/2026/07/0701/personal_submission/cf106607b_Meguhine.py
@@ -0,0 +1,24 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def solve() -> str:
+    n = int(input())
+    a = list(map(int, input().split()))
+    if max(a) == 1:
+        return "0"
+    for l in range(n):
+        if a[l] == 1:
+            continue
+        for r in range(l + 1, n):
+            if a[r] == 1:
+                return "1" if max(a[r:]) == 1 else "2"
+            if a[l] != a[r]:
+                return "2"
+        else:
+            return "1"
+    assert False, "Unrechable"
+
+
+print('\n'.join(solve() for _ in range(int(input()))))
