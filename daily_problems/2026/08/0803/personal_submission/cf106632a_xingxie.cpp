0b00c5df47013699839dae8b77a7c3c8c6c9421d
xingxie1 <1483281343@qq.com>
2026-08-04 00:29:21 +0800

    20260803 cf106632a personal submission

---
+++ daily_problems/2026/08/0803/personal_submission/cf106632a_xingxie.cpp
@@ -0,0 +1,98 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+using i128 = __int128_t;
+using vt = vector<int>;
+using vd = vector<double>;
+using vll = vector<long long>;
+using vvt = vector<vector<int>>;
+using vvd = vector<vector<double>>;
+using vvll = vector<vector<long long>>;
+using vvvt = vector<vector<vector<int>>>;
+using vvvll = vector<vector<vector<long long>>>;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using pdd = pair<double,double>;
+using vpii = vector<pair<int,int>>;
+using vpll = vector<pair<ll,ll>>;
+using vpdd = vector<pair<double,double>>;
+using vvpii = vector<vector<pair<int,int>>>;
+using vvpll = vector<vector<pair<ll,ll>>>;
+using tri = tuple<int,int,int>;
+using trl = tuple<ll,ll,ll>;
+using vtri = vector<tuple<int,int,int>>;
+using vtrl = vector<tuple<ll,ll,ll>>;
+#define YES cout << "YES" << endl
+#define Yes cout << "Yes" << endl
+#define NO cout << "NO" << endl
+#define No cout << "No" << endl
+#define fi first
+#define se second
+#define umap unordered_map
+#define uset unordered_set
+#define pqueue priority_queue
+#define mset multiset
+#define endl '\n'
+//const int MOD = 998244353;
+//const int MOD = (int)1e9+7;
+
+int N = 1000005; // 最大筛到 N
+vector<int> primes;// 存储质数
+vector<bool> isPrime(N + 1, true); // isPrime[i] = true 表示 i 是质数
+vector<int> min_p(N + 1);// i的最小质因子
+auto init = [] 
+{
+    isPrime[0] = isPrime[1] = false; 
+    for (int i = 2; i <= N; ++i) 
+    {
+        if (isPrime[i]) 
+        {
+            primes.push_back(i);
+            min_p[i] = i;
+        }
+        for (int p : primes) 
+        {
+            if (1LL * i * p > N) break; 
+            isPrime[i * p] = false; // 标记为合数
+            min_p[i * p] = p;
+            if (i % p == 0) break;  // 保证每个合数只被最小质因子筛掉一次
+        }
+    }
+    
+    return 0;
+}();
+
+void solve()
+{
+    int m;
+    cin >> m;
+    int cnt = 1;
+    for (int i = 2;i <= sqrt(m);i ++) 
+    {
+        if (isPrime[i]) 
+        {
+            ll x = i;
+            while (x * x <= m) 
+            {
+                x *= x;
+                cnt++;
+            }
+        }
+    }
+    cnt += ranges::lower_bound(primes,m + 1) - primes.begin();
+    cout << cnt << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << fixed << setprecision(15);
+    int _ = 1;
+    cin >> _;
+    while (_ --) solve();
+
+    return 0;
+}
