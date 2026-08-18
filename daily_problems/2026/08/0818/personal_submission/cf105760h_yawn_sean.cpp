2be75aaf08fb70e51919ecfe2c0ed789cf1ddb13
Yawn-Sean <1900015431@pku.edu.cn>
2026-08-18 21:09:43 +0800

    20260817 Yawn Sean's submission for CF105760H

--- daily_problems/2026/08/0818/personal_submission/cf105760h_yawn_sean.cpp
+++ daily_problems/2026/08/0818/personal_submission/cf105760h_yawn_sean.cpp
@@ -14,16 +14,31 @@ int main() {
 	cin.tie(0);
 	cout.tie(0);
 
-	int t;
-	cin >> t;
+	int k, s, e;
+	cin >> k >> s >> e;
 
-	while (t --) {
-		int n;
-		string s;
-		cin >> n >> s;
+	vector<int> emptys;
+	for (int i = 1; i <= k; i ++) {
+		if (i != s && i != e) {
+			emptys.emplace_back(i);
+		}
+	}
 
-		if (n & 1) cout << (s[n / 2] == 'B' && (s[n / 2 - 1] == 'B' || s[n / 2 + 1] == 'B') ? "Doludu" : "DoIudu") << '\n';
-		else cout << (s[n / 2 - 1] == 'B' || s[n / 2] == 'B' ? "Doludu" : "DoIudu") << '\n';
+	cout << 2 * (k - 1) * (k - 1) - 1 << '\n';
+
+	for (int i = k - 1; i >= 1; i --) {
+		cout << s << ' ' << e << '\n';
+		for (int j = 0; j <= i - 2; j ++) cout << s << ' ' << emptys[k - 3 - j] << '\n';
+		for (int j = i - 3; j >= 0; j --) cout << emptys[k - 3 - j] << ' ' << emptys[k - 1 - i] << '\n';
+		if (i > 1) cout << e << ' ' << emptys[k - 1 - i] << '\n';
+	}
+
+	for (int i = k - 3; i >= 0; i --) {
+		cout << emptys[i] << ' ' << s << '\n';
+		for (int j = k - 3; j >= i + 1; j --) cout << emptys[i] << ' ' << emptys[j] << '\n';
+		cout << emptys[i] << ' ' << e << '\n';
+		for (int j = i + 1; j <= k - 3; j ++) cout << emptys[j] << ' ' << e << '\n';
+		cout << s << ' ' << e << '\n';
 	}
 
 	return 0;
