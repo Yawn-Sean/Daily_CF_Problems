f7a17fd51079f039f62cd45689d4861e2e1ea3de
Yawn-Sean <1900015431@pku.edu.cn>
2026-09-01 21:53:19 +0800

    20260901 Yawn Sean's submission for CF105223G

--- daily_problems/2026/09/0902/personal_submission/cf105223g_yawn_sean.cpp
+++ daily_problems/2026/09/0902/personal_submission/cf105223g_yawn_sean.cpp
@@ -11,37 +11,43 @@ int main() {
 	cin.tie(0);
 	cout.tie(0);
 
-	auto query = [&] (int x) -> char {
-		cout << x << endl;
-		char c; cin >> c;
-		return c;
-	};
-
-	auto answer = [&] (int x1, int x2) -> void {
-		cout << "! " << x1 << ' ' << x2 << endl;
-	};
-
-	auto search = [&] (int l, int r) -> int {
-		while (l <= r) {
-			int mid = (l + r) / 2;
-			auto res = query(mid);
-
-			if (res == '=') return mid;
-			if (res == '>') l = mid + 1;
-			else r = mid - 1;
-		}
-
-		return -1;
-	};
-
 	int n;
 	cin >> n;
 
-	auto v1 = search(1, n);
-	auto v2 = max(search(1, v1 - 1), search(v1 + 1, n));
+	vector<int> nums(n), cnt(30, 0);
 
-	if (v1 > v2) swap(v1, v2);
-	answer(v1, v2);
+	for (auto &x: nums) {
+		cin >> x;
+		for (int i = 0; i < 30; i ++) {
+			cnt[i] += x >> i & 1;
+		}
+	}
+
+	int mod = 1e9 + 7, rev2 = (mod + 1) / 2;
+	vector<int> pw3(n + 1, 1);
+	for (int i = 0; i < n; i ++) pw3[i + 1] = 3ll * pw3[i] % mod;
+
+	int q;
+	cin >> q;
+
+	while (q --) {
+		int idx, val;
+		cin >> idx >> val;
+		idx --;
+
+		for (int i = 0; i < 30; i ++) cnt[i] -= nums[idx] >> i & 1;
+		nums[idx] = val;
+		for (int i = 0; i < 30; i ++) cnt[i] += nums[idx] >> i & 1;
+
+		int ans = 0;
+		for (int i = 29; i >= 0; i --) {
+			ans = 2 * ans % mod;
+			ans = (ans + pw3[n]) % mod;
+			ans = (ans + mod - pw3[n - cnt[i]]) % mod;
+		}
+
+		cout << 1ll * ans * rev2 % mod << '\n';
+	}
 
 	return 0;
 }
