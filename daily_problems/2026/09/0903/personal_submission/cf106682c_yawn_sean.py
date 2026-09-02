47dc8994d9143150838bcbd1e169dfe7f40c49c6
Yawn-Sean <1900015431@pku.edu.cn>
2026-09-02 22:40:03 +0800

    20260902 Yawn Sean's submission for CF106682C

--- daily_problems/2026/09/0903/personal_submission/cf106682c_yawn_sean.py
+++ daily_problems/2026/09/0903/personal_submission/cf106682c_yawn_sean.py
@@ -1,11 +1,12 @@
+# Submission link: https://codeforces.com/gym/106682/submission/389163140
 def main():
     n = II()
     v1 = [0] + LII()
     v2 = [0] + LII()
-
+    
     w1, w2 = v1[n], v2[n]
-
+    
     for i in range(n - 1, -1, -1):
         w1, w2 = fmax(w1, fmin(v1[i], w2)), fmin(w2, fmax(v2[i], w1))
-
+    
     print(w1)
