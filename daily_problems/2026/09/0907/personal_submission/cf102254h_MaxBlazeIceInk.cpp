94ba673449bd5a0170c71dcf9eb6a37be0af9f27
MaxBlazeResFire <3282036755@qq.com>
2026-09-07 14:27:47 +0800

    20260907_MaxBlazeIceInk

---
+++ daily_problems/2026/09/0907/personal_submission/cf102254h_MaxBlazeIceInk.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n,m;
+
+inline void solve(){
+	scanf("%d%d",&n,&m);
+	puts("0");
+}
+
+signed main(){
+	int t; scanf("%d",&t);
+	while( t -- ) solve();
+	return 0;
+}
