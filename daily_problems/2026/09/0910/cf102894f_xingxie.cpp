94c71e56949badca989c5da445769a70edac89d9
xingxie <1483281343@qq.com>
2026-09-10 22:38:52 +0800

    20260910 cf102894 personal submission

---
+++ daily_problems/2026/09/0910/cf102894f_xingxie.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+using i128 = __int128_t;
+using vt = vector<int>;
+using vd = vector<double>;
+using vll = vector<long long>;
+using vvt = vector<vector<int>>;
+using vvd = vector<vector<double>>;
+using vvll = vector<vector<long long>>;
+using vvvt = vector<vector<vector<int>>>;
+using vvvll = vector<vector<vector<long long>>>;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using pdd = pair<double,double>;
+using vpii = vector<pair<int,int>>;
+using vpll = vector<pair<ll,ll>>;
+using vpdd = vector<pair<double,double>>;
+using vvpii = vector<vector<pair<int,int>>>;
+using vvpll = vector<vector<pair<ll,ll>>>;
+using tri = tuple<int,int,int>;
+using trl = tuple<ll,ll,ll>;
+using vtri = vector<tuple<int,int,int>>;
+using vtrl = vector<tuple<ll,ll,ll>>;
+#define YES cout << "YES" << endl
+#define Yes cout << "Yes" << endl
+#define NO cout << "NO" << endl
+#define No cout << "No" << endl
+#define fi first
+#define se second
+#define umap unordered_map
+#define uset unordered_set
+#define pqueue priority_queue
+#define mset multiset
+#define endl '\n'
+//const int MOD = 998244353;
+//const int MOD = (int)1e9+7;
+
+void solve()
+{
+    int n,k;
+    cin >> n >> k;
+    vt a(n);
+    vpii b(k);
+    for (int i = 0;i < n;i++) cin >> a[i];
+    for (int i = 0;i < k;i++) cin >> b[i].fi;
+    for (int i = 0;i < k;i++) cin >> b[i].se;
+    ranges::sort(a);
+    ranges::sort(b);
+    ll ans = 0;
+    pqueue<int> pq;
+    int j = 0;
+    for (int i = 0;i < n;i++)
+    {
+        while (j < k && b[j].fi <= a[i]) 
+        {
+            pq.push(b[j].se);
+            j++;
+        }
+        if (!pq.empty())
+        {
+            ans += pq.top();
+            pq.pop();
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << fixed << setprecision(15);
+    int _ = 1;
+    // cin >> _;
+    while (_ --) solve();
+
+    return 0;
+}
