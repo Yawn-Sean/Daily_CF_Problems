40428e12b4952c576aeba4365027e118bb201040
xingxie <1483281343@qq.com>
2026-09-16 14:44:34 +0800

    20260916 cf106706n personal submission

---
+++ daily_problems/2026/09/0916/personal_submission/cf106706n_xingxie.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+using i128 = __int128_t;
+using vt = vector<int>;
+using vd = vector<double>;
+using vll = vector<long long>;
+using vvt = vector<vector<int>>;
+using vvd = vector<vector<double>>;
+using vvll = vector<vector<long long>>;
+using vvvt = vector<vector<vector<int>>>;
+using vvvll = vector<vector<vector<long long>>>;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using pdd = pair<double,double>;
+using vpii = vector<pair<int,int>>;
+using vpll = vector<pair<ll,ll>>;
+using vpdd = vector<pair<double,double>>;
+using vvpii = vector<vector<pair<int,int>>>;
+using vvpll = vector<vector<pair<ll,ll>>>;
+using tri = tuple<int,int,int>;
+using trl = tuple<ll,ll,ll>;
+using vtri = vector<tuple<int,int,int>>;
+using vtrl = vector<tuple<ll,ll,ll>>;
+#define YES cout << "YES" << endl
+#define Yes cout << "Yes" << endl
+#define NO cout << "NO" << endl
+#define No cout << "No" << endl
+#define fi first
+#define se second
+#define umap unordered_map
+#define uset unordered_set
+#define pqueue priority_queue
+#define mset multiset
+#define endl '\n'
+//const int MOD = 998244353;
+//const int MOD = (int)1e9+7;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vpii a(n),b(n);
+    for (int i = 0;i < n;i++) cin >> a[i].fi,a[i].se = i;
+    for (int i = 0;i < n;i++) cin >> b[i].fi,b[i].se = i;
+    ranges::sort(a,{},[&](auto& c){
+        return c.fi;
+    });
+    ranges::sort(b,{},[&](auto& c){
+        return -c.fi;
+    });
+    vt ans(n);
+    for (int i = 0;i < n;i++) ans[a[i].se] = b[i].se;
+    for (int x : ans) cout << x + 1 << " ";
+    cout << endl;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << fixed << setprecision(15);
+    int _ = 1;
+    cin >> _;
+    while (_ --) solve();
+
+    return 0;
+}
